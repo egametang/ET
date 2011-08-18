b2e38cdd508b8d5f585dc73060eae7d5dd7761e0
tanghai <egametang@gmail.com>
2011-08-19 01:04:55 +0800

    去掉editor目录,qt很难用,准备用wpf写编辑器

--- Cpp/Editor/Ui/Main.cc
+++
@@ -1,9 +0,0 @@
-#include <QtCore/QCoreApplication>
-#include <QtCore/QDebug>
-
-int main(int argc, char** argv)
-{
-	QCoreApplication app(argc, argv);
-	qDebug() << "hello qt!";
-	return app.exec();
-}
