700fe2916fd0d5da3cd1add10e8fd3fc0c93137a
tanghai <egametang@gmail.com>
2011-09-15 19:33:44 +0800

    treeview放到一个自定义控件中

---
+++ CSharp/GameEditor/BehaviorTreeView.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Egametang
+{
+	/// <summary>
+	/// BehaviorTreeView.xaml 的交互逻辑
+	/// </summary>
+	public partial class BehaviorTreeView : UserControl
+	{
+		public BehaviorTreeView()
+		{
+			InitializeComponent();
+		}
+
+		private void newMenuItem_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void copyMenuItem_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void pasteMenuItem_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void delMenuItem_Click(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void behaviorTreeView_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+		{
+
+		}
+	}
+}
---
+++ CSharp/ViewModel/BehaviorNodeViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Egametang
+{
+	public class BehaviorNodeViewModel : TreeViewItemViewModel
+	{
+		private readonly BehaviorNode node;
+
+		public BehaviorNodeViewModel(BehaviorNode node, BehaviorNodeViewModel parent) :
+			base(parent, false)
+		{
+			this.node = node;
+		}
+
+		public string Name
+		{
+			get
+			{
+				return node.Name;
+			}
+		}
+
+		public virtual void LoadChildren()
+		{
+			foreach (var child in node.Children)
+			{
+				base.Children.Add(new BehaviorNodeViewModel(child, this));
+			}
+		}
+	}
+}
