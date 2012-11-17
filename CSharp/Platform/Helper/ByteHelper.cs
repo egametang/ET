e3e47c0fe4e9f51c148b6b3952f2fa01d54f6066
tanghai <egametang@gmail.com>
2012-11-17 14:31:54 +0800

    增加了两个扩展类,方便日志输出string的hex格式

---
+++ CSharp/Platform/Helper/ByteHelper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Helper
+{
+	public static class ByteHelper
+	{
+		public static string ToHex(this byte b)
+		{
+		    return b.ToString("X2");
+		}
+		
+		public static string ToHex(this IEnumerable<byte> bytes)
+		{
+		    var stringBuilder = new StringBuilder();
+			foreach (byte b in bytes)
+			{
+				stringBuilder.Append(b.ToString("X2"));
+			}
+			return stringBuilder.ToString();
+		}
+	}
+}
---
+++ CSharp/Platform/Helper/StringHelper.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Helper
+{
+	public static class StringHelper
+	{
+		public static IEnumerable<byte> ToBytes(this string str)
+		{
+			byte[] byteArray = Encoding.Default.GetBytes(str);
+			return byteArray;
+		}
+
+		public static string ToHex(this string str)
+		{
+			IEnumerable<byte> byteArray = str.ToBytes();
+			return byteArray.ToHex();
+		}
+	}
+}
