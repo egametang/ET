cf43cf977d56bf5d507a6215c2c09277cdbb459c
tanghai <egametang@gmail.com>
2013-03-08 20:27:50 +0800

    修复WCF运行错误,Contract接口函数需要OperationContract属性

--- CSharp/Contract/RealmContract/ICalculator.cs
+++ CSharp/Contract/RealmContract/ICalculator.cs
@@ -5,6 +5,7 @@ namespace RealmContract
 	[ServiceContract(Name = "Calculator", Namespace = "http://www.egametang.com/")]
 	public interface ICalculator
 	{
+		[OperationContract]
 		double Add(double x, double y);
 	}
 }
