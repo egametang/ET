ff9e7d2a68d6a0f8011547befa5cecf5f2d425e3
tanghai <egametang@gmail.com>
2013-08-05 13:49:52 +0800

    删除不用的using和库引用

--- CSharp/App/BossMonit/Protocol.cs
+++ CSharp/App/BossMonit/Protocol.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Runtime.Serialization;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BossMonit
 {
