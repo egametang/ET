780903434dc816852ea7d333db1d81ea08613dd3
tanghai <egametang@gmail.com>
2014-05-28 18:31:44 +0800

    C# 异步tcp库

---
+++ CSharp/Platform/TNet/TcpAcceptor.cs
@@ -0,0 +1,72 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TNet
+{
+    public class TcpAcceptor: IDisposable
+    {
+	    private readonly Socket socket;
+		private readonly SocketAsyncEventArgs asyncEventArgs = new SocketAsyncEventArgs();
+
+	    public TcpAcceptor(string ip, ushort port, int backLog = 100)
+	    {
+			this.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+			this.asyncEventArgs.Completed += OnArgsCompletion;
+			this.socket.Bind(new IPEndPoint(IPAddress.Parse(ip), port));
+			this.socket.Listen(backLog);
+	    }
+
+		public void Dispose()
+		{
+			this.socket.Dispose();
+		}
+
+		/// <summary>
+		/// SocketAsyncEventArgs Accept回调函数,回调可能已经到了另外的线程
+		/// </summary>
+		/// <returns></returns>
+	    public Task<NetworkStream> AcceptAsync()
+	    {
+			var tcs = new TaskCompletionSource<NetworkStream>();
+		    
+			try
+			{
+				asyncEventArgs.UserToken = tcs;
+
+				bool ret = this.socket.AcceptAsync(asyncEventArgs);
+
+				if (!ret)
+				{
+					if (asyncEventArgs.SocketError == SocketError.Success)
+					{
+						var acceptSocket = asyncEventArgs.AcceptSocket;
+						asyncEventArgs.AcceptSocket = null;
+						return Task.FromResult(new NetworkStream(acceptSocket, true));
+					}
+					tcs.TrySetException(new InvalidOperationException(this.asyncEventArgs.SocketError.ToString()));
+				}
+			}
+			catch (Exception ex)
+			{
+				tcs.TrySetException(ex);
+			}
+			return tcs.Task;
+	    }
+
+	    private static void OnArgsCompletion(object sender, SocketAsyncEventArgs e)
+	    {
+			var tcs = (TaskCompletionSource<NetworkStream>)e.UserToken;
+
+			if (e.SocketError != SocketError.Success)
+			{
+				tcs.TrySetException(new InvalidOperationException(e.SocketError.ToString()));
+				return;
+			}
+			var acceptSocket = e.AcceptSocket;
+			e.AcceptSocket = null;
+			tcs.SetResult(new NetworkStream(acceptSocket, true));
+	    }
+    }
+}
---
+++ CSharp/Platform/TNetTest/TcpAcceptorTest.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using Helper;
+using Logger;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TNet;
+
+namespace TNetTest
+{
+	[TestClass]
+	public class TcpAcceptorTest
+	{
+		private const ushort port = 11111;
+		private int count;
+		private readonly Barrier barrier = new Barrier(2);
+		private readonly object lockObject = new object();
+
+		[TestMethod]
+		public void AcceptAsync()
+		{
+			var thread1 = new Thread(Server);
+			thread1.Start();
+
+			Thread.Sleep(2);
+
+			for (int i = 0; i < 99; ++i)
+			{
+				var thread = new Thread(this.Client);
+				thread.Start();
+			}
+			barrier.SignalAndWait();
+		}
+
+		private async void Client()
+		{
+			using (var tcpClient = new TcpClient(AddressFamily.InterNetwork))
+			{
+				await tcpClient.ConnectAsync("127.0.0.1", port);
+				using (NetworkStream ns = tcpClient.GetStream())
+				{
+					try
+					{
+						var bytes = "tanghai".ToByteArray();
+						for (int i = 0; i < 100000; ++i)
+						{
+							await ns.WriteAsync(bytes, 0, bytes.Length);
+							int n = await ns.ReadAsync(bytes, 0, bytes.Length);
+							Assert.AreEqual(7, n);
+						}
+					}
+					catch (Exception e)
+					{
+						Log.Debug(e.ToString());
+					}
+				}
+			}
+			barrier.RemoveParticipants(1);
+		}
+
+		private async void Server()
+		{
+			using (var tcpAcceptor = new TcpAcceptor("127.0.0.1", port))
+			{
+				while (count != 99)
+				{
+					Log.Debug("start server response");
+					NetworkStream ns = await tcpAcceptor.AcceptAsync();
+					// 这里可能已经不在Server函数线程了
+					Log.Debug("server response");
+					Response(ns);
+				}
+			}
+		}
+
+		private async void Response(NetworkStream ns)
+		{
+			try
+			{
+				var bytes = new byte[1000];
+				for (int i = 0; i < 100000; ++i)
+				{
+					int n = await ns.ReadAsync(bytes, 0, 100);
+					await ns.WriteAsync(bytes, 0, n);
+				}
+				lock (lockObject)
+				{
+					++count;
+				}
+			}
+			catch (Exception e)
+			{
+				Log.Debug(e.ToString());
+			}
+		}
+	}
+}
