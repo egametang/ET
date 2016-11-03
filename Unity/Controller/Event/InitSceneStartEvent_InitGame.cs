841bdbf48284ebead322e09924472368311d6e51
tanghai <egametang@gmail.com>
2016-11-03 16:39:41 +0800

    async void异步方法必须加上try catch

--- Unity/Controller/Event/InitSceneStartEvent_InitGame.cs
+++ Unity/Controller/Event/InitSceneStartEvent_InitGame.cs
@@ -12,32 +12,39 @@ namespace Controller
 	{
 		public async void Run()
 		{
-			Game.Scene.AddComponent<MessageDispatherComponent, AppType>(AppType.Client);
-			ClientConfig clientConfig = Game.Scene.AddComponent<ClientConfigComponent>().Config.GetComponent<ClientConfig>();
-			NetOuterComponent networkComponent = Game.Scene.AddComponent<NetOuterComponent>();
-			using (Session session = networkComponent.Create(clientConfig.Address))
+			try
 			{
-				try
+				Game.Scene.AddComponent<MessageDispatherComponent, AppType>(AppType.Client);
+				ClientConfig clientConfig = Game.Scene.AddComponent<ClientConfigComponent>().Config.GetComponent<ClientConfig>();
+				NetOuterComponent networkComponent = Game.Scene.AddComponent<NetOuterComponent>();
+				using (Session session = networkComponent.Create(clientConfig.Address))
 				{
-					R2C_Login s2CLogin = await session.Call<C2R_Login, R2C_Login>(new C2R_Login { Account = "abcdef", Password = "111111" });
-					networkComponent.Remove(session.Id);
-
-					// 连接Gate
-					using (Session gateSession = networkComponent.Create(s2CLogin.Address))
+					try
 					{
-						await gateSession.Call<C2G_LoginGate, G2C_LoginGate>(new C2G_LoginGate(s2CLogin.Key));
+						R2C_Login s2CLogin = await session.Call<C2R_Login, R2C_Login>(new C2R_Login { Account = "abcdef", Password = "111111" });
+						networkComponent.Remove(session.Id);
+
+						// 连接Gate
+						using (Session gateSession = networkComponent.Create(s2CLogin.Address))
+						{
+							await gateSession.Call<C2G_LoginGate, G2C_LoginGate>(new C2G_LoginGate(s2CLogin.Key));
+						}
+						
+						Log.Info("连接Gate验证成功!");
 					}
-					
-					Log.Info("连接Gate验证成功!");
+					catch (RpcException e)
+					{
+						Log.Error(e.ToString());
+					}
+					catch (Exception e)
+					{
+						Log.Error(e.ToString());
+					}
 				}
-				catch (RpcException e)
-				{
-					Log.Error(e.ToString());
-				}
-				catch (Exception e)
-				{
-					Log.Error(e.ToString());
-				}
+			}
+			catch (Exception e)
+			{
+				Log.Error(e.ToString());
 			}
 		}
 	}
