8338c604f60b545175d52f0e9602c7666980e7c2
tanghai <egametang@gmail.com>
2017-06-29 18:31:02 +0800

    删除不用的代码

--- Server/Model/Other/Options.cs
+++ Server/Model/Other/Options.cs
@@ -10,19 +10,13 @@ namespace Model
 {
 	public class Options: ICloneable
 	{
-#if SERVER
 		[Option("appId", Required = true)]
-#endif
 		public int AppId { get; set; }
-
-#if SERVER
+		
 		[Option("appType", Required = true)]
-#endif
 		public AppType AppType { get; set; }
 
-#if SERVER
 		[Option("config", Required = false, DefaultValue = "Start.txt")]
-#endif
 		public string Config { get; set; }
 
 		public object Clone()
