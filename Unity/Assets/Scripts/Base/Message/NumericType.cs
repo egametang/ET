d2f4add03549a770dd0c4b139a1c43fe659cc54a
tanghai <egametang@gmail.com>
2017-09-26 15:47:01 +0800

    添加一个数值组件，一个数值监听组件

--- Unity/Assets/Scripts/Base/Message/NumericType.cs
+++ Unity/Assets/Scripts/Base/Message/NumericType.cs
@@ -10,5 +10,15 @@
 	    SpeedPct = Speed * 10 + 3,
 	    SpeedFinalAdd = Speed * 10 + 4,
 	    SpeedFinalPct = Speed * 10 + 5,
+
+	    Hp = 1001,
+	    HpBase = Hp * 10 + 1,
+
+	    MaxHp = 1002,
+	    MaxHpBase = MaxHp * 10 + 1,
+	    MaxHpAdd = MaxHp * 10 + 2,
+	    MaxHpPct = MaxHp * 10 + 3,
+	    MaxHpFinalAdd = MaxHp * 10 + 4,
+		MaxHpFinalPct = MaxHp * 10 + 5,
 	}
 }
