1c6fe57f9c11f0d84d398b9df8127ca2e1bd8af0
tanghai <egametang@gmail.com>
2017-11-18 10:58:24 +0800

    默认不启用ASYNC

--- Unity/Assets/Scripts/Helper/BundleHelper.cs
+++ Unity/Assets/Scripts/Helper/BundleHelper.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 using UnityEngine;
 
@@ -20,31 +17,14 @@ namespace Model
 		{
 			if (Define.IsAsync)
 			{
-				//string url = GlobalConfigComponent.Instance.GlobalProto.GetUrl() + "StreamingAssets/StreamingAssets";
-				//try
-				//{
-				//	using (WWWAsync wwwAsync = EntityFactory.Create<WWWAsync>())
-				//	{
-				//		await wwwAsync.DownloadAsync(url);
-				//		ResourcesComponent.AssetBundleManifestObject = wwwAsync.www.assetBundle.LoadAsset<AssetBundleManifest>("AssetBundleManifest");
-				//		wwwAsync.www.assetBundle.Unload(false);
-				//	}
-				//}
-				//catch (Exception e)
-				//{
-				//	Log.Error($"下载错误: {url} {e}");
-				//	return;
-				//}
 				try
 				{
 					using (BundleDownloaderComponent bundleDownloaderComponent = Game.Scene.AddComponent<BundleDownloaderComponent>())
 					{
 						await bundleDownloaderComponent.StartAsync();
 					}
-					Log.Debug("11111111111111111111111111111111");
 					Game.Scene.GetComponent<ResourcesComponent>().LoadOneBundle("StreamingAssets");
 					ResourcesComponent.AssetBundleManifestObject = Game.Scene.GetComponent<ResourcesComponent>().GetAsset<AssetBundleManifest>("StreamingAssets", "AssetBundleManifest");
-					Log.Debug("111111111111111111111111111111112");
 				}
 				catch (Exception e)
 				{
