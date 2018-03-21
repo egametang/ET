8feb5ac1695bcd83c97c518191a9c8e8bcc908b4
tanghai <egametang@gmail.com>
2018-03-21 15:02:29 +0800

    整理了代码

--- Server/Hotfix/Module/Http/HttpTest.cs
+++ Server/Hotfix/Module/Http/HttpTest.cs
@@ -1,55 +1,49 @@
-﻿using ETModel;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Net;
-using System.Text;
+﻿using System.Net;
+using ETModel;
 
 namespace ETHotfix
 {
+	public class AccountInfo
+	{
+		public string name;
+		public string pwd;
+	}
 
-  public class AccountInfo
-  {
-    public string name;
-    public string pwd;
-  }
+	[HttpHandler(AppType.Gate, "/")]
+	public class HttpTest: AHttpHandler
+	{
+		[Get] // url-> /Login?name=11&age=1111
+		public string Login(string name, int age, HttpListenerRequest req, HttpListenerResponse resp)
+		{
+			Log.Info(name);
+			Log.Info($"{age}");
+			return "ok";
+		}
 
-  [HttpHandler(AppType.Gate, "/")]
-  public class HttpTest : AHttpHandler
-  {
-    [Get] // url-> /Login?name=11&age=1111
-    public string Login(string name, int age, HttpListenerRequest req, HttpListenerResponse resp)
-    {
-      Log.Info(name);
-      Log.Info($"{age}");
-      return "ok";
-    }
+		[Get("t")] // url-> /t
+		public int Test()
+		{
+			return 1;
+		}
 
-    [Get("t")] // url-> /t
-    public int Test()
-    {
-      return 1;
-    }
+		[Post] // url-> /Test1
+		public int Test1(HttpListenerRequest req)
+		{
+			return 1;
+		}
 
-    [Post] // url-> /Test1
-    public int Test1(HttpListenerRequest req)
-    {
-      return 1;
-    }
+		[Get] // url-> /Test2
+		public int Test2(HttpListenerResponse resp)
+		{
+			return 1;
+		}
 
-    [Get] // url-> /Test2
-    public int Test2(HttpListenerResponse resp)
-    {
-      return 1;
-    }
-
-    [Post] 
-    public object Test3(HttpListenerResponse resp, HttpListenerRequest req, string postBody, AccountInfo accountInfo)
-    {
-      Log.Info(postBody);
-      Log.Info(JsonHelper.ToJson(accountInfo));
-      return new { name = "1111" };
-    }
-  }
+		[Post]
+		public object Test3(HttpListenerResponse resp, HttpListenerRequest req, string postBody, AccountInfo accountInfo)
+		{
+			Log.Info(postBody);
+			Log.Info(JsonHelper.ToJson(accountInfo));
+			return new { name = "1111" };
+		}
+	}
 }
- 
--- Server/Model/Module/Http/HttpComponent.cs
+++ Server/Model/Module/Http/HttpComponent.cs
@@ -1,305 +1,317 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Reflection;
-using System.IO;
 
 namespace ETModel
 {
-  [ObjectSystem]
-  public class HttpComponentComponentAwakeSystem : AwakeSystem<HttpComponent>
-  {
-    public override void Awake(HttpComponent self)
-    {
-      self.Awake();
-    }
-  }
+	[ObjectSystem]
+	public class HttpComponentComponentAwakeSystem: AwakeSystem<HttpComponent>
+	{
+		public override void Awake(HttpComponent self)
+		{
+			self.Awake();
+		}
+	}
 
-  [ObjectSystem]
-  public class HttpComponentComponentLoadSystem : LoadSystem<HttpComponent>
-  {
-    public override void Load(HttpComponent self)
-    {
-      self.Load();
-    }
-  }
+	[ObjectSystem]
+	public class HttpComponentComponentLoadSystem: LoadSystem<HttpComponent>
+	{
+		public override void Load(HttpComponent self)
+		{
+			self.Load();
+		}
+	}
 
-  /// <summary>
-  /// http请求分发器
-  /// </summary>
-  public class HttpComponent : Component
-  {
-    public AppType appType;
-    public HttpListener listener;
-    public HttpConfig HttpConfig;
-    public Dictionary<string, IHttpHandler> dispatcher;
+	/// <summary>
+	/// http请求分发器
+	/// </summary>
+	public class HttpComponent: Component
+	{
+		public AppType appType;
+		public HttpListener listener;
+		public HttpConfig HttpConfig;
+		public Dictionary<string, IHttpHandler> dispatcher;
 
-    // 处理方法
-    private Dictionary<MethodInfo, IHttpHandler> handlersMapping;
+		// 处理方法
+		private Dictionary<MethodInfo, IHttpHandler> handlersMapping;
 
-    // Get处理
-    private Dictionary<string, MethodInfo> getHandlers;
-    private Dictionary<string, MethodInfo> postHandlers;
+		// Get处理
+		private Dictionary<string, MethodInfo> getHandlers;
+		private Dictionary<string, MethodInfo> postHandlers;
 
-    public void Awake()
-    {
-      StartConfig startConfig = Game.Scene.GetComponent<StartConfigComponent>().StartConfig;
-      this.appType = startConfig.AppType;
-      this.HttpConfig = startConfig.GetComponent<HttpConfig>();
+		public void Awake()
+		{
+			StartConfig startConfig = Game.Scene.GetComponent<StartConfigComponent>().StartConfig;
+			this.appType = startConfig.AppType;
+			this.HttpConfig = startConfig.GetComponent<HttpConfig>();
 
-      this.Load();
+			this.Load();
 
-      try
-      {
-        this.listener = new HttpListener();
+			try
+			{
+				this.listener = new HttpListener();
 
-        if (this.HttpConfig.Url == null)
-        {
-          this.HttpConfig.Url = "";
-        }
-        foreach (string s in this.HttpConfig.Url.Split(';'))
-        {
-          if (s.Trim() == "")
-          {
-            continue;
-          }
-          this.listener.Prefixes.Add(s);
-        }
+				if (this.HttpConfig.Url == null)
+				{
+					this.HttpConfig.Url = "";
+				}
 
-        this.listener.Start();
+				foreach (string s in this.HttpConfig.Url.Split(';'))
+				{
+					if (s.Trim() == "")
+					{
+						continue;
+					}
 
-        this.Accept();
-      }
-      catch (HttpListenerException e)
-      {
-        throw new Exception($"http server error: {e.ErrorCode}", e);
-      }
-    }
+					this.listener.Prefixes.Add(s);
+				}
 
-    public void Load()
-    {
-      this.dispatcher = new Dictionary<string, IHttpHandler>();
-      this.handlersMapping = new Dictionary<MethodInfo, IHttpHandler>();
-      this.getHandlers = new Dictionary<string, MethodInfo>();
-      this.postHandlers = new Dictionary<string, MethodInfo>();
+				this.listener.Start();
 
-      Type[] types = DllHelper.GetMonoTypes();
+				this.Accept();
+			}
+			catch (HttpListenerException e)
+			{
+				throw new Exception($"http server error: {e.ErrorCode}", e);
+			}
+		}
 
-      foreach (Type type in types)
-      {
-        object[] attrs = type.GetCustomAttributes(typeof(HttpHandlerAttribute), false);
-        if (attrs.Length == 0)
-        {
-          continue;
-        }
+		public void Load()
+		{
+			this.dispatcher = new Dictionary<string, IHttpHandler>();
+			this.handlersMapping = new Dictionary<MethodInfo, IHttpHandler>();
+			this.getHandlers = new Dictionary<string, MethodInfo>();
+			this.postHandlers = new Dictionary<string, MethodInfo>();
 
-        HttpHandlerAttribute httpHandlerAttribute = (HttpHandlerAttribute)attrs[0];
-        if (!httpHandlerAttribute.AppType.Is(this.appType))
-        {
-          continue;
-        }
+			Type[] types = DllHelper.GetMonoTypes();
 
-        object obj = Activator.CreateInstance(type);
+			foreach (Type type in types)
+			{
+				object[] attrs = type.GetCustomAttributes(typeof(HttpHandlerAttribute), false);
+				if (attrs.Length == 0)
+				{
+					continue;
+				}
 
-        IHttpHandler ihttpHandler = obj as IHttpHandler;
-        if (ihttpHandler == null)
-        {
-          throw new Exception($"HttpHandler handler not inherit IHttpHandler class: {obj.GetType().FullName}");
-        }
+				HttpHandlerAttribute httpHandlerAttribute = (HttpHandlerAttribute) attrs[0];
+				if (!httpHandlerAttribute.AppType.Is(this.appType))
+				{
+					continue;
+				}
 
-        this.dispatcher.Add(httpHandlerAttribute.Path, ihttpHandler);
+				object obj = Activator.CreateInstance(type);
 
-        LoadMethod(type, httpHandlerAttribute, ihttpHandler);
-      }
-    }
+				IHttpHandler ihttpHandler = obj as IHttpHandler;
+				if (ihttpHandler == null)
+				{
+					throw new Exception($"HttpHandler handler not inherit IHttpHandler class: {obj.GetType().FullName}");
+				}
 
-    public void LoadMethod(Type type, HttpHandlerAttribute httpHandlerAttribute, IHttpHandler httpHandler)
-    {
-      // 扫描这个类里面的方法
-      MethodInfo[] methodInfos = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.InvokeMethod | BindingFlags.Instance);
-      foreach (var method in methodInfos)
-      {
-        object[] getAttrs = method.GetCustomAttributes(typeof(GetAttribute), false);
-        if (getAttrs.Length != 0)
-        {
-          GetAttribute get = (GetAttribute)getAttrs[0];
+				this.dispatcher.Add(httpHandlerAttribute.Path, ihttpHandler);
 
-          string path = method.Name;
-          if (!string.IsNullOrEmpty(get.Path))
-            path = get.Path;
+				LoadMethod(type, httpHandlerAttribute, ihttpHandler);
+			}
+		}
 
-          getHandlers.Add(httpHandlerAttribute.Path + path, method);
-          Log.Debug($"add handler[{httpHandler.ToString()}.{method.Name}] path {httpHandlerAttribute.Path + path}");
-        }
+		public void LoadMethod(Type type, HttpHandlerAttribute httpHandlerAttribute, IHttpHandler httpHandler)
+		{
+			// 扫描这个类里面的方法
+			MethodInfo[] methodInfos = type.GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.InvokeMethod | BindingFlags.Instance);
+			foreach (MethodInfo method in methodInfos)
+			{
+				object[] getAttrs = method.GetCustomAttributes(typeof(GetAttribute), false);
+				if (getAttrs.Length != 0)
+				{
+					GetAttribute get = (GetAttribute) getAttrs[0];
 
-        object[] postAttrs = method.GetCustomAttributes(typeof(PostAttribute), false);
-        if (postAttrs.Length != 0)
-        {
-          // Post处理方法
-          PostAttribute post = (PostAttribute)postAttrs[0];
+					string path = method.Name;
+					if (!string.IsNullOrEmpty(get.Path))
+					{
+						path = get.Path;
+					}
 
-          string path = method.Name;
-          if (!string.IsNullOrEmpty(post.Path))
-            path = post.Path;
+					getHandlers.Add(httpHandlerAttribute.Path + path, method);
+					//Log.Debug($"add handler[{httpHandler}.{method.Name}] path {httpHandlerAttribute.Path + path}");
+				}
 
-          postHandlers.Add(httpHandlerAttribute.Path + path, method);
-          Log.Debug($"add handler[{httpHandler.ToString()}.{method.Name}] path {httpHandlerAttribute.Path + path}");
-        }
+				object[] postAttrs = method.GetCustomAttributes(typeof(PostAttribute), false);
+				if (postAttrs.Length != 0)
+				{
+					// Post处理方法
+					PostAttribute post = (PostAttribute) postAttrs[0];
 
-        if (getAttrs.Length == 0 && postAttrs.Length == 0) continue;
+					string path = method.Name;
+					if (!string.IsNullOrEmpty(post.Path))
+					{
+						path = post.Path;
+					}
 
-        handlersMapping.Add(method, httpHandler);
-      }
-    }
+					postHandlers.Add(httpHandlerAttribute.Path + path, method);
+					//Log.Debug($"add handler[{httpHandler}.{method.Name}] path {httpHandlerAttribute.Path + path}");
+				}
 
-    public async void Accept()
-    {
-      while (true)
-      {
-        if (this.IsDisposed)
-        {
-          return;
-        }
+				if (getAttrs.Length == 0 && postAttrs.Length == 0)
+				{
+					continue;
+				}
 
-        HttpListenerContext context = await this.listener.GetContextAsync();
-        InvokeHandler(context);
-        context.Response.Close();
-      }
-    }
+				handlersMapping.Add(method, httpHandler);
+			}
+		}
 
-    /// <summary>
-    /// 调用处理方法
-    /// </summary>
-    /// <param name="context"></param>
-    private void InvokeHandler(HttpListenerContext context)
-    {
-      context.Response.StatusCode = 404;
+		public async void Accept()
+		{
+			while (true)
+			{
+				if (this.IsDisposed)
+				{
+					return;
+				}
 
-      MethodInfo methodInfo = null;
-      IHttpHandler httpHandler = null;
-      string postbody = "";
-      if (context.Request.HttpMethod == "GET")
-      {
-        this.getHandlers.TryGetValue(context.Request.Url.AbsolutePath, out methodInfo);
-        if (methodInfo != null)
-          this.handlersMapping.TryGetValue(methodInfo, out httpHandler);
-      }
-      else if (context.Request.HttpMethod == "POST")
-      {
-        this.postHandlers.TryGetValue(context.Request.Url.AbsolutePath, out methodInfo);
-        if (methodInfo != null)
-        {
-          this.handlersMapping.TryGetValue(methodInfo, out httpHandler);
+				HttpListenerContext context = await this.listener.GetContextAsync();
+				InvokeHandler(context);
+				context.Response.Close();
+			}
+		}
 
-          using (StreamReader sr = new StreamReader(context.Request.InputStream))
-          {
-            postbody = sr.ReadToEnd();
-          }
-        }
-      }
-      else
-      {
-        context.Response.StatusCode = 405;
-      }
+		/// <summary>
+		/// 调用处理方法
+		/// </summary>
+		/// <param name="context"></param>
+		private void InvokeHandler(HttpListenerContext context)
+		{
+			context.Response.StatusCode = 404;
 
-      if (httpHandler != null)
-      {
-        object[] args = InjectParameters(context, methodInfo, postbody);
+			MethodInfo methodInfo = null;
+			IHttpHandler httpHandler = null;
+			string postbody = "";
+			switch (context.Request.HttpMethod)
+			{
+				case "GET":
+					this.getHandlers.TryGetValue(context.Request.Url.AbsolutePath, out methodInfo);
+					if (methodInfo != null)
+					{
+						this.handlersMapping.TryGetValue(methodInfo, out httpHandler);
+					}
+					break;
+				case "POST":
+					this.postHandlers.TryGetValue(context.Request.Url.AbsolutePath, out methodInfo);
+					if (methodInfo != null)
+					{
+						this.handlersMapping.TryGetValue(methodInfo, out httpHandler);
 
-        // 自动把返回值，以json方式响应。
-        object resp = methodInfo?.Invoke(httpHandler, args);
-        if (resp != null)
-        {
-          using (StreamWriter sw = new StreamWriter(context.Response.OutputStream))
-          {
-            if (resp.GetType() == typeof(string))
-            {
-              sw.Write(resp.ToString());
-            }
-            else
-            {
-              sw.Write(JsonHelper.ToJson(resp));
-            }
-          }
-        }
-      }
-    }
+						using (StreamReader sr = new StreamReader(context.Request.InputStream))
+						{
+							postbody = sr.ReadToEnd();
+						}
+					}
+					break;
+				default:
+					context.Response.StatusCode = 405;
+					break;
+			}
 
-    /// <summary>
-    /// 注入参数
-    /// </summary>
-    /// <param name="context"></param>
-    /// <param name="methodInfo"></param>
-    /// <param name="postbody"></param>
-    /// <returns></returns>
-    private static object[] InjectParameters(HttpListenerContext context, MethodInfo methodInfo, string postbody)
-    {
-      context.Response.StatusCode = 200;
-      ParameterInfo[] parameterInfos = methodInfo.GetParameters();
-      object[] args = new object[parameterInfos.Length];
-      for (int i = 0; i < parameterInfos.Length; i++)
-      {
-        ParameterInfo item = parameterInfos[i];
+			if (httpHandler != null)
+			{
+				object[] args = InjectParameters(context, methodInfo, postbody);
 
-        if (item.ParameterType == typeof(HttpListenerRequest))
-        {
-          args[i] = context.Request;
-        }
-        else if (item.ParameterType == typeof(HttpListenerResponse))
-        {
-          args[i] = context.Response;
-        }
-        else
-        {
-          try
-          {
-            if (context.Request.HttpMethod == "POST") //TODO 扩展一些，Http Entity 自动转换 的功能
-            {
-              if (item.Name == "postBody") // 约定参数名称为postBody,只传string类型。本来是byte[]，有需求可以改。
-              {
-                args[i] = postbody;
-              }
-              else if (item.ParameterType.IsClass && item.ParameterType != typeof(string) && !string.IsNullOrEmpty(postbody))
-              {
-                object entity = JsonHelper.FromJson(item.ParameterType, postbody);
-                args[i] = entity;
-              }
-            }
-            else if (context.Request.HttpMethod == "GET")
-            {
-              string query = context.Request.QueryString[item.Name];
-              if (query != null)
-              {
-                object value = Convert.ChangeType(query, item.ParameterType);
-                args[i] = value;
-              }
-            }
-            else
-            {
-              args[i] = null;
-            }
-          }
-          catch (Exception e)
-          {
-            Log.Debug(e.ToString());
-            args[i] = null;
-          }
-        }
-      }
-      return args;
-    }
+				// 自动把返回值，以json方式响应。
+				object resp = methodInfo.Invoke(httpHandler, args);
+				if (resp != null)
+				{
+					using (StreamWriter sw = new StreamWriter(context.Response.OutputStream))
+					{
+						if (resp is string)
+						{
+							sw.Write(resp.ToString());
+						}
+						else
+						{
+							sw.Write(JsonHelper.ToJson(resp));
+						}
+					}
+				}
+			}
+		}
 
-    public override void Dispose()
-    {
-      if (this.IsDisposed)
-      {
-        return;
-      }
+		/// <summary>
+		/// 注入参数
+		/// </summary>
+		/// <param name="context"></param>
+		/// <param name="methodInfo"></param>
+		/// <param name="postbody"></param>
+		/// <returns></returns>
+		private static object[] InjectParameters(HttpListenerContext context, MethodInfo methodInfo, string postbody)
+		{
+			context.Response.StatusCode = 200;
+			ParameterInfo[] parameterInfos = methodInfo.GetParameters();
+			object[] args = new object[parameterInfos.Length];
+			for (int i = 0; i < parameterInfos.Length; i++)
+			{
+				ParameterInfo item = parameterInfos[i];
 
-      base.Dispose();
+				if (item.ParameterType == typeof(HttpListenerRequest))
+				{
+					args[i] = context.Request;
+				}
+				else if (item.ParameterType == typeof(HttpListenerResponse))
+				{
+					args[i] = context.Response;
+				}
+				else
+				{
+					try
+					{
+						if (context.Request.HttpMethod == "POST") //TODO 扩展一些，Http Entity 自动转换 的功能
+						{
+							if (item.Name == "postBody") // 约定参数名称为postBody,只传string类型。本来是byte[]，有需求可以改。
+							{
+								args[i] = postbody;
+							}
+							else if (item.ParameterType.IsClass && item.ParameterType != typeof(string) && !string.IsNullOrEmpty(postbody))
+							{
+								object entity = JsonHelper.FromJson(item.ParameterType, postbody);
+								args[i] = entity;
+							}
+						}
+						else if (context.Request.HttpMethod == "GET")
+						{
+							string query = context.Request.QueryString[item.Name];
+							if (query != null)
+							{
+								object value = Convert.ChangeType(query, item.ParameterType);
+								args[i] = value;
+							}
+						}
+						else
+						{
+							args[i] = null;
+						}
+					}
+					catch (Exception e)
+					{
+						Log.Debug(e.ToString());
+						args[i] = null;
+					}
+				}
+			}
 
-      this.listener.Stop();
-      this.listener.Close();
-    }
-  }
+			return args;
+		}
+
+		public override void Dispose()
+		{
+			if (this.IsDisposed)
+			{
+				return;
+			}
+
+			base.Dispose();
+
+			this.listener.Stop();
+			this.listener.Close();
+		}
+	}
 }
