498595ca9261817b511c1a132c0eb15d4f11f6d9
Yinmany <qq918482974@163.com>
2018-03-22 15:34:23 +0800

    1. 修复一个Http模块中，异步处理方法的bug。

--- Server/Model/Entity/Http.cs
+++ Server/Model/Entity/Http.cs
@@ -1,45 +1,47 @@
 ﻿namespace ETModel
 {
-	// 充值流水
-	public sealed class RechargeRecord : Entity
-	{
-		// 充值玩家
-		public int PlayerNO { get; set; }
+  // 充值流水
+  public sealed class RechargeRecord : Entity
+  {
+    // 充值玩家
+    public int PlayerNO { get; set; }
 
-		// 充值数量
-		public int CardNumber { get; set; }
-		
-		// 充值时间
-		public long Time { get; set; }
+    // 充值数量
+    public int CardNumber { get; set; }
 
-		public RechargeRecord(long id): base(id)
-		{
-		}
-	}
+    // 充值时间
+    public long Time { get; set; }
 
-	// 保存玩家充值记录, 每个玩家只有一条
-	public sealed class Recharge : Entity
-	{
-		public int CardNumber { get; set; }
-		
-		public long UpdateTime { get; set; }
+    public RechargeRecord(long id) : base(id)
+    {
+    }
+  }
 
-		public Recharge(long id): base(id)
-		{
-		}
-	}
+  // 保存玩家充值记录, 每个玩家只有一条
+  public sealed class Recharge : Entity
+  {
+    public int CardNumber { get; set; }
 
-	public class HttpResult
-	{
-		public int code;
-		public bool status;
-		public string msg = "";
-	}
+    public long UpdateTime { get; set; }
 
-	public static class HttpErrorCode
-	{
-		public const int Exception = 999;
-		public const int Success = 1000;
-		public const int RpcFail = 1002;
-	}
+    public Recharge(long id) : base(id)
+    {
+    }
+  }
+
+  public class HttpResult
+  {
+    public int code;
+    public bool status;
+    public string msg = "";
+    [MongoDB.Bson.Serialization.Attributes.BsonIgnoreIfNull]
+    public object data;
+  }
+
+  public static class HttpErrorCode
+  {
+    public const int Exception = 999;
+    public const int Success = 1000;
+    public const int RpcFail = 1002;
+  }
 }
