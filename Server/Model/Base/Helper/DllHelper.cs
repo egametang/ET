5b9f3fe32fe258153389c17065792edd540449c3
tanghai <egametang@gmail.com>
2018-08-10 19:57:30 +0800

    server不再兼容mono，dll加载也不需要区分mono了

--- Server/Model/Base/Helper/DllHelper.cs
+++ Server/Model/Base/Helper/DllHelper.cs
@@ -10,11 +10,7 @@ namespace ETModel
 		public static Assembly GetHotfixAssembly()
 		{
 			byte[] dllBytes = File.ReadAllBytes("./Hotfix.dll");
-#if __MonoCS__
-			byte[] pdbBytes = File.ReadAllBytes("./Hotfix.dll.mdb");
-#else
 			byte[] pdbBytes = File.ReadAllBytes("./Hotfix.pdb");
-#endif
 			Assembly assembly = Assembly.Load(dllBytes, pdbBytes);
 			return assembly;
 		}
