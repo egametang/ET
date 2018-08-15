feb5676b868726cd1eb6d93b62214cba18d328b8
tanghai <egametang@gmail.com>
2018-08-15 20:07:18 +0800

    增加ECS多组件筛选功能

--- Unity/Assets/Scripts/Base/Object/Entity.cs
+++ Unity/Assets/Scripts/Base/Object/Entity.cs
@@ -52,7 +52,7 @@ namespace ETModel
 			this.componentDict.Clear();
 		}
 		
-		public Component AddComponent(Component component)
+		public virtual Component AddComponent(Component component)
 		{
 			Type type = component.GetType();
 			if (this.componentDict.ContainsKey(type))
@@ -70,7 +70,7 @@ namespace ETModel
 			return component;
 		}
 
-		public Component AddComponent(Type type)
+		public virtual Component AddComponent(Type type)
 		{
 			if (this.componentDict.ContainsKey(type))
 			{
@@ -87,7 +87,7 @@ namespace ETModel
 			return component;
 		}
 
-		public K AddComponent<K>() where K : Component, new()
+		public virtual K AddComponent<K>() where K : Component, new()
 		{
 			Type type = typeof (K);
 			if (this.componentDict.ContainsKey(type))
@@ -105,7 +105,7 @@ namespace ETModel
 			return component;
 		}
 
-		public K AddComponent<K, P1>(P1 p1) where K : Component, new()
+		public virtual K AddComponent<K, P1>(P1 p1) where K : Component, new()
 		{
 			Type type = typeof (K);
 			if (this.componentDict.ContainsKey(type))
@@ -123,7 +123,7 @@ namespace ETModel
 			return component;
 		}
 
-		public K AddComponent<K, P1, P2>(P1 p1, P2 p2) where K : Component, new()
+		public virtual K AddComponent<K, P1, P2>(P1 p1, P2 p2) where K : Component, new()
 		{
 			Type type = typeof (K);
 			if (this.componentDict.ContainsKey(type))
@@ -141,7 +141,7 @@ namespace ETModel
 			return component;
 		}
 
-		public K AddComponent<K, P1, P2, P3>(P1 p1, P2 p2, P3 p3) where K : Component, new()
+		public virtual K AddComponent<K, P1, P2, P3>(P1 p1, P2 p2, P3 p3) where K : Component, new()
 		{
 			Type type = typeof (K);
 			if (this.componentDict.ContainsKey(type))
@@ -159,7 +159,7 @@ namespace ETModel
 			return component;
 		}
 
-		public void RemoveComponent<K>() where K : Component
+		public virtual void RemoveComponent<K>() where K : Component
 		{
 			if (this.IsDisposed)
 			{
@@ -178,7 +178,7 @@ namespace ETModel
 			component.Dispose();
 		}
 
-		public void RemoveComponent(Type type)
+		public virtual void RemoveComponent(Type type)
 		{
 			if (this.IsDisposed)
 			{
---
+++ Unity/Assets/Scripts/Module/Filter/FilterAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ETModel
+{
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+	public class FilterAttribute: BaseAttribute
+	{
+		// 指定管理器的类型名
+		public string ManagerTypeName { get; }
+
+		public FilterAttribute(string managerTypeName)
+		{
+			this.ManagerTypeName = managerTypeName;
+		}
+	}
+}
