d31a4e4c6b737d9bf83c712e52a4ebd179b6b0ed
tanghai <egametang@gmail.com>
2018-09-25 11:36:07 +0800

    去掉两行注释，这两行注释描述错误，服务端也应该使用这两个接口

--- Unity/Assets/Model/Module/ActorLocation/IActorLocationMessage.cs
+++ Unity/Assets/Model/Module/ActorLocation/IActorLocationMessage.cs
@@ -1,11 +1,9 @@
 ﻿namespace ETModel
 {
-	// 客户端发送actor消息
 	public interface IActorLocationMessage : IActorRequest
 	{
 	}
 
-	// 客户端发送actor rpc消息
 	public interface IActorLocationRequest : IActorRequest
 	{
 	}
