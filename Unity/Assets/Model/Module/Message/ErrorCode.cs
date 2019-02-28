01b4a17ca9862a710d9ad62613fb96db8aa9d211
tanghai <egametang@gmail.com>
2019-02-28 14:12:28 +0800

    整理了ErrorCode

--- Unity/Assets/Model/Module/Message/ErrorCode.cs
+++ Unity/Assets/Model/Module/Message/ErrorCode.cs
@@ -9,37 +9,40 @@ namespace ETModel
 		// 100000 以上，避免跟SocketError冲突
 		public const int ERR_MyErrorCode = 100000;
 		
+		public const int ERR_ActorNoMailBoxComponent = 100003;
+		public const int ERR_ActorRemove = 100004;
+		public const int ERR_PacketParserError = 100005;
+		
+		
+		public const int ERR_SessionActorError = 100103;
+		public const int ERR_NotFoundUnit = 100104;
+		public const int ERR_ConnectGateKeyError = 100105;
 
-		// 小于这个Rpc会抛异常
+		public const int ERR_RpcFail = 102001;
+		public const int ERR_SocketDisconnected = 102002;
+		public const int ERR_ReloadFail = 102003;
+		public const int ERR_ActorLocationNotFound = 102004;
+		public const int ERR_KcpCantConnect = 102005;
+		public const int ERR_KcpChannelTimeout = 102006;
+		public const int ERR_KcpRemoteDisconnect = 102007;
+		public const int ERR_PeerDisconnect = 102008;
+		public const int ERR_SocketCantSend = 102009;
+		public const int ERR_SocketError = 102010;
+		public const int ERR_KcpWaitSendSizeTooLarge = 102011;
+
+		public const int ERR_WebsocketPeerReset = 103001;
+		public const int ERR_WebsocketMessageTooBig = 103002;
+		public const int ERR_WebsocketError = 103003;
+		public const int ERR_WebsocketConnectError = 103004;
+		public const int ERR_WebsocketSendError = 103005;
+
+		// 小于这个Rpc会抛异常，大于这个异常的error需要自己判断处理，也就是说需要处理的错误应该要大于该值
 		public const int ERR_Exception = 200000;
 		
 		public const int ERR_NotFoundActor = 200002;
-		public const int ERR_ActorNoMailBoxComponent = 200003;
-		public const int ERR_ActorRemove = 200004;
-		public const int ERR_PacketParserError = 200005;
+
 
 		public const int ERR_AccountOrPasswordError = 200102;
-		public const int ERR_SessionActorError = 200103;
-		public const int ERR_NotFoundUnit = 200104;
-		public const int ERR_ConnectGateKeyError = 200105;
-
-		public const int ERR_RpcFail = 202001;
-		public const int ERR_SocketDisconnected = 202002;
-		public const int ERR_ReloadFail = 202003;
-		public const int ERR_ActorLocationNotFound = 202004;
-		public const int ERR_KcpCantConnect = 202005;
-		public const int ERR_KcpChannelTimeout = 202006;
-		public const int ERR_KcpRemoteDisconnect = 202007;
-		public const int ERR_PeerDisconnect = 202008;
-		public const int ERR_SocketCantSend = 202009;
-		public const int ERR_SocketError = 202010;
-		public const int ERR_KcpWaitSendSizeTooLarge = 202011;
-
-		public const int ERR_WebsocketPeerReset = 203001;
-		public const int ERR_WebsocketMessageTooBig = 203002;
-		public const int ERR_WebsocketError = 203003;
-		public const int ERR_WebsocketConnectError = 203004;
-		public const int ERR_WebsocketSendError = 203005;
 		public const int ERR_WebsocketRecvError = 203006;
 
 		public static bool IsRpcNeedThrowException(int error)
