e2c172709daf0928401d012f4060acf79f1de1c8
ks <1227114700@qq.com>
2019-03-25 13:23:50 +0800

    优化对象池可视化GameObject

--- Unity/Assets/Model/Base/Object/ObjectPool.cs
+++ Unity/Assets/Model/Base/Object/ObjectPool.cs
@@ -96,6 +96,9 @@ namespace ETModel
             {
                 queue = new ComponentQueue(type.Name);
 	            queue.Parent = this;
+#if !SERVER
+	            queue.GameObject.name = type.Name;
+#endif
 				this.dictionary.Add(type, queue);
             }
             queue.Enqueue(obj);
