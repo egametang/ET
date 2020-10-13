a2f6a8922bc455928f627c6ab32e418c7d265731
JianFengL <34735413+JianFengL@users.noreply.github.com>
2020-10-13 13:42:16 +0800

    FileServer部分修改
    删除部分包引用，netcore3.0以上不需要再引入Microsoft.AspNetCore.App

--- Tools/FileServer/Program.cs
+++ Tools/FileServer/Program.cs
@@ -1,26 +1,26 @@
-﻿using System.IO;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using System.IO;
 
 
 namespace ETFileServer
 {
     public class Program
     {
+        public static IConfigurationRoot mConfig;
         public static void Main(string[] args)
         {
+            mConfig = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true)
+                .Build();
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", true)
-                .Build();
-            
-            return WebHost.CreateDefaultBuilder(args).UseUrls(config["urls"])
+            return WebHost.CreateDefaultBuilder(args).UseUrls(mConfig["urls"])
                 .UseStartup<Startup>();
         }
     }
