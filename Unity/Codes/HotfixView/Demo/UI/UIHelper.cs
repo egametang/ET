ba06d2c46624829fe71b8297cef2a9c7ab4ac20d
tanghai <egametang@gmail.com>
2021-11-22 18:13:46 +0800

    修复编译错误

---
+++ Unity/Codes/HotfixView/Demo/UI/UIHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace ET
+{
+    public static class UIHelper
+    {
+        public static async ETTask<UI> Create(Scene scene, string uiType)
+        {
+            return await scene.GetComponent<UIComponent>().Create(uiType);
+        }
+        
+        public static async ETTask Remove(Scene scene, string uiType)
+        {
+            scene.GetComponent<UIComponent>().Remove(uiType);
+            await ETTask.CompletedTask;
+        }
+    }
+}
