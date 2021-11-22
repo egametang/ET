ec251b7886539e7ae53f847ded290f674a073341
tanghai <egametang@gmail.com>
2021-11-22 20:28:41 +0800

    导表生成代码到Codes目录，csproj引用文件改成引用目录

--- Server/Model/ExcelExporter/ExcelExporter.cs
+++ Server/Model/ExcelExporter/ExcelExporter.cs
@@ -39,7 +39,7 @@ namespace ET
     {
         private static string template;
 
-        private const string clientClassDir = "../Unity/Assets/Model/Generate/Config";
+        private const string clientClassDir = "../Unity/Codes/Model/Generate/Config";
         private const string serverClassDir = "../Server/Model/Generate/Config";
         
         private const string excelDir = "../Excel";
