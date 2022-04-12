e6d06faa64ca4ee8e5cf82760c8a0d22997ce658
wenchao <susices@hotmail.com>
2022-04-12 20:25:49 +0800

    增加两个Analyzer
    1.Hotfix程序集中 不允许声明非Const字段
    
    2.Hotfix程序集中 只能声明含有BaseAttribute子类特性的类或静态类。即只能声明生命周期类 分发类 静态类。

--- Tools/Analyzer/Analyzer/EntityClassDeclarationAnalyzer.cs
+++ Tools/Analyzer/Analyzer/EntityClassDeclarationAnalyzer.cs
@@ -1,22 +1,20 @@
 ﻿using System.Collections.Immutable;
-using System.Linq;
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace ET.Analyzer
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public class EntityClassDeclarationAnalyzer : DiagnosticAnalyzer
+    public class EntityClassDeclarationAnalyzer: DiagnosticAnalyzer
     {
         private const string Title = "实体类限制多层继承";
-        
+
         private const string MessageFormat = "类: {0} 不能继承Entiy的子类 请直接继承Entity";
-        
+
         private const string Description = "实体类限制多层继承.";
 
         private const string EntityType = "ET.Entity";
-        
+
         private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticIds.EntityClassDeclarationAnalyzerRuleId,
             Title,
             MessageFormat,
@@ -24,36 +22,35 @@ namespace ET.Analyzer
             DiagnosticSeverity.Error, true, Description);
 
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(Rule);
-        
+
         public override void Initialize(AnalysisContext context)
         {
             if (!AnalyzerGlobalSetting.EnableAnalyzer)
             {
                 return;
             }
+
             context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
             context.EnableConcurrentExecution();
             context.RegisterSymbolAction(this.Analyzer, SymbolKind.NamedType);
         }
-        
+
         private void Analyzer(SymbolAnalysisContext context)
         {
-
             if (!(context.Symbol is INamedTypeSymbol namedTypeSymbol))
             {
                 return;
             }
-            
-            
+
             if (namedTypeSymbol.BaseType?.BaseType?.ToString() != EntityType)
             {
                 return;
             }
 
-            foreach (var declaringSyntaxReference in namedTypeSymbol.DeclaringSyntaxReferences)
+            foreach (SyntaxReference? declaringSyntaxReference in namedTypeSymbol.DeclaringSyntaxReferences)
             {
-                var classSyntax = declaringSyntaxReference.GetSyntax();
-                Diagnostic diagnostic =Diagnostic.Create(Rule,  classSyntax.GetLocation(), namedTypeSymbol.Name, context.Compilation.AssemblyName);
+                SyntaxNode classSyntax = declaringSyntaxReference.GetSyntax();
+                Diagnostic diagnostic = Diagnostic.Create(Rule, classSyntax.GetLocation(), namedTypeSymbol.Name, context.Compilation.AssemblyName);
                 context.ReportDiagnostic(diagnostic);
             }
         }
