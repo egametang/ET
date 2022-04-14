e01350dedc5b96c069759720b381c28cbe2aec26
tanghai <egametang@gmail.com>
2022-04-14 10:02:26 +0800

    客户端的订阅消息都改成同步的，做mmo，消息量大的时候这块ETTask的gc也不能忽视

--- Unity/Codes/Hotfix/Demo/Unit/M2C_CreateMyUnitHandler.cs
+++ Unity/Codes/Hotfix/Demo/Unit/M2C_CreateMyUnitHandler.cs
@@ -3,11 +3,10 @@
 	[MessageHandler]
 	public class M2C_CreateMyUnitHandler : AMHandler<M2C_CreateMyUnit>
 	{
-		protected override async ETTask Run(Session session, M2C_CreateMyUnit message)
+		protected override void Run(Session session, M2C_CreateMyUnit message)
 		{
 			// 通知场景切换协程继续往下走
 			session.DomainScene().GetComponent<ObjectWait>().Notify(new WaitType.Wait_CreateMyUnit() {Message = message});
-			await ETTask.CompletedTask;
 		}
 	}
 }
