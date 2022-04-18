fb1dacb6e91b3497edb06cc8b0832612f7752f4e
tanghai <egametang@gmail.com>
2022-04-18 20:36:30 +0800

    默认打开日志

--- Unity/Assets/Mono/MonoBehaviour/Init.cs
+++ Unity/Assets/Mono/MonoBehaviour/Init.cs
@@ -40,7 +40,7 @@ namespace ET
 
 			CodeLoader.Instance.CodeMode = this.CodeMode;
 			Options.Instance.Develop = 1;
-			Options.Instance.LogLevel = 6;
+			Options.Instance.LogLevel = 0;
 		}
 
 		private void Start()
