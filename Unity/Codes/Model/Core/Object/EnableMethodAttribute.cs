6b8ade3dc2087c84ad45a9089bedf94197831c8f
susices <susices@hotmail.com>
2022-05-05 16:54:23 +0800

    添加EnableMethodAttribute 允许标记该特性的实体类内声明方法 (#343)
    * 添加EnableMethodAttribute 允许标记该特性的实体类内声明方法

---
+++ Unity/Codes/Model/Core/Object/EnableMethodAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ET
+{
+    /// <summary>
+    /// 对于特殊实体类 允许类内部声明方法的标签
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class EnableMethodAttribute : Attribute
+    {
+        
+    }
+}
