9a954b259ed6756ed6fd5c7f6e3db67a91b65301
tanghai <egametang@gmail.com>
2022-06-09 16:38:11 +0800

    修复Apps Model跟Hotfix没有应用上分析器的Bug

--- Share/Analyzer/Config/AnalyzeAssembly.cs
+++ Share/Analyzer/Config/AnalyzeAssembly.cs
@@ -2,9 +2,9 @@
 {
     public static class AnalyzeAssembly
     {
-        public const string ServerModel = "Model";
+        public const string AppsModel = "Apps.Model";
 
-        public const string ServerHotfix = "Hotfix";
+        public const string AppsHotfix = "Apps.Hotfix";
 
         public const string UnityModel = "Unity.Model";
 
@@ -14,10 +14,10 @@
 
         public const string UnityHotfixView = "Unity.HotfixView";
 
-        public static readonly string[] AllHotfix = { ServerHotfix, UnityHotfix, UnityHotfixView };
+        public static readonly string[] AllHotfix = { AppsHotfix, UnityHotfix, UnityHotfixView };
 
-        public static readonly string[] AllModel = { ServerModel, UnityModel, UnityModelView };
+        public static readonly string[] AllModel = { AppsModel, UnityModel, UnityModelView };
 
-        public static readonly string[] All = { ServerModel, ServerHotfix, UnityModel, UnityHotfix, UnityModelView, UnityHotfixView };
+        public static readonly string[] All = { AppsModel, AppsHotfix, UnityModel, UnityHotfix, UnityModelView, UnityHotfixView };
     }
 }
