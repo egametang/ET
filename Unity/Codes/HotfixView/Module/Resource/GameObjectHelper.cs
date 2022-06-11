4266a7e4cbfe50d28ed20ed8c8d076552c5d47f8
Carson Lin <44496710+kteong1012@users.noreply.github.com>
2022-06-11 14:37:54 +0800

    feat: 优化RC，支持改名字，添加获取组件扩展方法GetComponentFormRC (#363)
    * feat: 优化RC，支持改名字，添加获取组件扩展方法GetComponentFormRC

--- Unity/Codes/HotfixView/Module/Resource/GameObjectHelper.cs
+++ Unity/Codes/HotfixView/Module/Resource/GameObjectHelper.cs
@@ -3,18 +3,33 @@ using UnityEngine;
 
 namespace ET
 {
-	public static class GameObjectHelper
-	{
-		public static T Get<T>(this GameObject gameObject, string key) where T : class
-		{
-			try
-			{
-				return gameObject.GetComponent<ReferenceCollector>().Get<T>(key);
-			}
-			catch (Exception e)
-			{
-				throw new Exception($"获取{gameObject.name}的ReferenceCollector key失败, key: {key}", e);
-			}
-		}
-	}
+    public static class GameObjectHelper
+    {
+        public static T Get<T>(this GameObject gameObject, string key) where T : class
+        {
+            try
+            {
+                return gameObject.GetComponent<ReferenceCollector>().Get<T>(key);
+            }
+            catch (Exception e)
+            {
+                throw new Exception($"获取{gameObject.name}的ReferenceCollector key失败, key: {key}", e);
+            }
+        }
+        public static T GetComponentFormRC<T>(this GameObject gameObject, string key) where T : Component
+        {
+            var gob = gameObject.Get<GameObject>(key);
+            if (gob == null)
+            {
+                Debug.LogWarning($"{gameObject.name}找不到物体{key}");
+                return null;
+            }
+            var com = gob.GetComponent<T>();
+            if (com == null)
+            {
+                Debug.LogWarning($"{gameObject.name}的{key}物体找不到{typeof(T).Name}组件");
+            }
+            return com;
+        }
+    }
 }
