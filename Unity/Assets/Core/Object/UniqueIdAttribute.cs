e9330aa377a8dad94f5e27bf6d76adcba793c586
wenchao <susices@hotmail.com>
2022-06-28 01:01:17 +0800

    添加EntityDelegateDeclarationAnalyzer UniqueIdAnalyzer 新增UniqueId标签
    1.EntityDelegateDeclarationAnalyzer
    禁止在实体类中声明委托字段和属性成员的约束
    2.UniqueIdAnalyzer
    新增UniqueIdAttribute, 被该标签标记的类会约束成员的const int 字段在类内部唯一 且在指定的数值区间内

---
+++ Unity/Assets/Core/Object/UniqueIdAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ET
+{
+    /// <summary>
+    /// 唯一Id标签
+    /// 使用此标签标记的类 会检测类内部的 const int 字段成员是否唯一
+    /// 可以指定唯一Id的最小值 最大值区间
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public class UniqueIdAttribute : Attribute
+    {
+        public int Min;
+
+        public int Max;
+        
+        public UniqueIdAttribute(int min = int.MinValue, int max = int.MaxValue)
+        {
+            this.Min = min;
+            this.Max = max;
+        }
+    }
+}
+
