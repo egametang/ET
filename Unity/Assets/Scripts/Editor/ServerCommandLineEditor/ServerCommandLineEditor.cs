4f5d59a7f3ac26a3389be40f51738304234cb23a
L <401419353@qq.com>
2022-08-06 02:33:19 +0800

    调整 Editor Window Dock 规则

--- Unity/Assets/Scripts/Editor/ServerCommandLineEditor/ServerCommandLineEditor.cs
+++ Unity/Assets/Scripts/Editor/ServerCommandLineEditor/ServerCommandLineEditor.cs
@@ -18,7 +18,7 @@ namespace ET
         [MenuItem("ET/ServerTools")]
         public static void ShowWindow()
         {
-            GetWindow(typeof (ServerCommandLineEditor));
+            GetWindow<ServerCommandLineEditor>(DockDefine.Types);
         }
         
         private int selectStartConfigIndex;
@@ -38,17 +38,23 @@ namespace ET
             this.startConfig = this.startConfigs[this.selectStartConfigIndex];
             this.developMode = (DevelopMode) EditorGUILayout.EnumPopup("起服模式：", this.developMode);
             int develop = (int) this.developMode;
+
+            string dotnet = "dotnet.exe";
+            
+#if UNITY_EDITOR_OSX
+            dotnet = "dotnet";
+#endif
             
             if (GUILayout.Button("Start Server(Single Srocess)"))
             {
                 string arguments = $"App.dll --Process=1 --StartConfig=StartConfig/{this.startConfig} --Console=1";
-                ProcessHelper.Run("dotnet.exe", arguments, "../Bin/");
+                ProcessHelper.Run(dotnet, arguments, "../Bin/");
             }
             
             if (GUILayout.Button("Start Watcher"))
             {
                 string arguments = $"App.dll --AppType=Watcher --StartConfig=StartConfig/{this.startConfig} --Console=1";
-                ProcessHelper.Run("dotnet.exe", arguments, "../Bin/");
+                ProcessHelper.Run(dotnet, arguments, "../Bin/");
             }
 
             if (GUILayout.Button("Start Mongo"))
