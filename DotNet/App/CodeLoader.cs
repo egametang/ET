f68d50db00ac2346581291d9021dc6a88f38c2ac
tanghai <egametang@gmail.com>
2022-08-13 23:50:20 +0800

    服务端Model跟Hotfix完全共享，DotNet相关代码挪到DotNet.App程序集

--- DotNet/App/CodeLoader.cs
+++ DotNet/App/CodeLoader.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Reflection;
 using System.Runtime.Loader;
 
-namespace ET
+namespace ET.Server
 {
     public class CodeLoader
     {
@@ -15,13 +15,13 @@ namespace ET
         public void LoadHotfix()
         {
             assemblyLoadContext?.Unload();
-            System.GC.Collect();
+            GC.Collect();
             assemblyLoadContext = new AssemblyLoadContext("Hotfix", true);
             byte[] dllBytes = File.ReadAllBytes("./Hotfix.dll");
             byte[] pdbBytes = File.ReadAllBytes("./Hotfix.pdb");
             Assembly assembly = assemblyLoadContext.LoadFromStream(new MemoryStream(dllBytes), new MemoryStream(pdbBytes));
             
-            Dictionary<string, Type> types = AssemblyHelper.GetAssemblyTypes(typeof (Game).Assembly, this.GetType().Assembly, assembly);
+            Dictionary<string, Type> types = AssemblyHelper.GetAssemblyTypes(typeof(Program).Assembly, typeof (Game).Assembly, typeof(Entry).Assembly, assembly);
 			
             Game.EventSystem.Add(types);
         }
--- DotNet/App/ReloadDllConsoleHandler.cs
+++ DotNet/App/ReloadDllConsoleHandler.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-
 namespace ET.Server
 {
     [ConsoleHandler(ConsoleMode.ReloadDll)]
