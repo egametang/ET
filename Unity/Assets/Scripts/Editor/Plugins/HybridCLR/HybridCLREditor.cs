6c3413852db228426f40ca733387892db062a7e7
tanghai <egametang@gmail.com>
2022-12-30 22:49:21 +0800

    补充元数据的dll配置到HybridCLRSettings.asset里面

--- Unity/Assets/Scripts/Editor/Plugins/HybridCLR/HybridCLREditor.cs
+++ Unity/Assets/Scripts/Editor/Plugins/HybridCLR/HybridCLREditor.cs
@@ -7,32 +7,20 @@ namespace ET
 {
     public static class HybridCLREditor
     {
-        private static readonly List<string> AotDlls = new()
-        {
-            "Unity.ThirdParty.dll", 
-            "Unity.Loader.dll", 
-            "Unity.Core.dll", 
-            "MongoDB.Bson.dll",
-            "CommandLine.dll",
-            "NLog.dll",
-            "mscorlib.dll",
-            "System.dll",
-            "System.Core.dll",
-        };
-        
         [MenuItem("HybridCLR/CopyAotDlls")]
         public static void CopyAotDll()
         {
             BuildTarget target = EditorUserBuildSettings.activeBuildTarget;
             string fromDir = Path.Combine(HybridCLRSettings.Instance.strippedAOTDllOutputRootDir, target.ToString());
             string toDir = "Assets/Bundles/AotDlls";
-            if (!Directory.Exists(toDir))
+            if (Directory.Exists(toDir))
             {
-                Directory.CreateDirectory(toDir);
+                Directory.Delete(toDir, true);
             }
+            Directory.CreateDirectory(toDir);
             AssetDatabase.Refresh();
             
-            foreach (string aotDll in AotDlls)
+            foreach (string aotDll in HybridCLRSettings.Instance.patchAOTAssemblies)
             {
                 File.Copy(Path.Combine(fromDir, aotDll), Path.Combine(toDir, $"{aotDll}.bytes"), true);
             }
