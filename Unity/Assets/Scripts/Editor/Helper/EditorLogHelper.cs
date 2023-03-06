44ffaea7e7d2e175580907ac58f2fb42d58028d4
肖红 <704872627@qq.com>
2023-03-06 13:01:19 +0800

    修复Log的bug
    支持EditorMode和多线程使用

---
+++ Unity/Assets/Scripts/Editor/Helper/EditorLogHelper.cs
@@ -0,0 +1,62 @@
+﻿using UnityEditor;
+
+namespace ET
+{
+    [InitializeOnLoad]
+    public class EditorLogHelper
+    {
+        static EditorLogHelper()
+        {
+            EditorApplication.playModeStateChanged -= OnPlayModeStateChanged;
+            EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+            EditorApplication.update += CheckCompolingFinish;
+        }
+
+        private static void CheckCompolingFinish()
+        {
+            if (!EditorApplication.isCompiling)
+            {
+                CreateLog();
+                EditorApplication.update -= CheckCompolingFinish;
+            }
+        }
+
+        private static void OnPlayModeStateChanged(PlayModeStateChange state)
+        {
+            switch (state)
+            {
+                case PlayModeStateChange.EnteredEditMode:
+                case PlayModeStateChange.ExitingPlayMode:
+                    CreateLog();
+                    break;
+                case PlayModeStateChange.ExitingEditMode:
+                    DestroyLog();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void CreateLog()
+        {
+            if (Logger.Instance != null)
+            {
+                return;
+            }
+
+            var log = new Logger();
+            ((ISingleton)log).Register();
+            log.ILog = new UnityLogger();
+        }
+
+        private static void DestroyLog()
+        {
+            if (Logger.Instance == null)
+            {
+                return;
+            }
+
+            ((ISingleton)Logger.Instance).Destroy();
+        }
+    }
+}
