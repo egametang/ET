455bd0a75e016a5e3e00b9955962e0156d01763a
tanghai <egametang@gmail.com>
2023-03-21 13:08:13 +0800

    增加第二种RobotCase

--- Unity/Assets/Scripts/Codes/Model/Server/Module/RobotCase/RobotCase.cs
+++ Unity/Assets/Scripts/Codes/Model/Server/Module/RobotCase/RobotCase.cs
@@ -1,9 +1,12 @@
-﻿namespace ET.Server
+﻿using System.Collections.Generic;
+
+namespace ET.Server
 {
     [ChildOf(typeof(RobotCaseComponent))]
-    public class RobotCase: Entity, IAwake
+    public class RobotCase: Entity, IAwake, IDestroy
     {
         public ETCancellationToken CancellationToken;
         public string CommandLine;
+        public HashSet<long> Scenes { get; } = new HashSet<long>();
     }
 }
