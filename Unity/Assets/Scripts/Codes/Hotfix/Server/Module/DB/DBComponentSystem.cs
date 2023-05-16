875db05b9c38f32b7d3a2ea49ac6c94af26dd6cf
BCC <30926304+kenkinky@users.noreply.github.com>
2023-05-16 09:04:05 +0800

    修正: 数据库存储与查询使用的默认集合名字不一致, 导致查询失败的BUG (#464)

--- Unity/Assets/Scripts/Codes/Hotfix/Server/Module/DB/DBComponentSystem.cs
+++ Unity/Assets/Scripts/Codes/Hotfix/Server/Module/DB/DBComponentSystem.cs
@@ -19,7 +19,7 @@ namespace ET.Server
 
 	    private static IMongoCollection<T> GetCollection<T>(this DBComponent self, string collection = null)
 	    {
-		    return self.database.GetCollection<T>(collection ?? typeof (T).Name);
+		    return self.database.GetCollection<T>(collection ?? typeof (T).FullName);
 	    }
 
 	    private static IMongoCollection<Entity> GetCollection(this DBComponent self, string name)
@@ -114,7 +114,7 @@ namespace ET.Server
 	    {
 		    if (collection == null)
 		    {
-			    collection = typeof (T).Name;
+			    collection = typeof (T).FullName;
 		    }
 		    
 		    using (await CoroutineLockComponent.Instance.Wait(CoroutineLockType.DB, RandomGenerator.RandInt64() % DBComponent.TaskCount))
