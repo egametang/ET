304d5d6e7e8bbe10b3433004fd0d6d2f80654f80
circle <iiccttff@163.com>
2023-07-14 00:28:43 +0800

    yooasset 适配 et task

---
+++ Unity/Assets/Scripts/Loader/Helper/YooAssetHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Networking;
+using YooAsset;
+
+namespace ET
+{
+    public static class YooAssetHelper
+    {
+        // 有了这个方法，就可以直接await Unity的AssetOperationHandle了
+        public static async ETTask GetAwaiter(this AssetOperationHandle asyncOperation)
+        {
+            ETTask task = ETTask.Create(true);
+            asyncOperation.Completed += _ => { task.SetResult(); };
+            await task;
+        }
+        
+        public static async ETTask GetAwaiter(this SceneOperationHandle asyncOperation)
+        {
+            ETTask task = ETTask.Create(true);
+            asyncOperation.Completed += _ => { task.SetResult(); };
+            await task;
+        }
+        
+    }
+}
