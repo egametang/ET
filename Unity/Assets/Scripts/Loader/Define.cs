b06ccddce2c6316a58f57e4ccf0905eacfec6950
EP-Toushirou <45257277+EP-Toushirou@users.noreply.github.com>
2023-11-07 17:54:27 +0800

    优化编辑器下dll加载路径
    1.将dll加载路径改成"Assets/Bundles/Code/"，避免重启Unity后必须编译才能运行
    2.修复初次打开项目时OnGeneratedCSProject的globalConfig会报空的问题
    3.加入文档注释和删除旧的无引用代码

--- Unity/Assets/Scripts/Loader/Define.cs
+++ Unity/Assets/Scripts/Loader/Define.cs
@@ -1,70 +1,51 @@
 ﻿namespace ET
 {
-	public static class Define
-	{
-		public const string CodeDir = "Assets/Bundles/Code/";
-		public const string BuildOutputDir = "Temp/Bin/Debug";
+    public static class Define
+    {
+        /// <summary>
+        /// 编辑器下加载热更dll的目录
+        /// </summary>
+        public const string CodeDir = "Assets/Bundles/Code/";
+
+        /// <summary>
+        /// VS或Rider工程生成dll的所在目录, 使用HybridCLR打包时需要使用
+        /// </summary>
+        public const string BuildOutputDir = "Temp/Bin/Debug";
+
 #if DEBUG
-		public static bool IsDebug = true;
+        public static bool IsDebug = true;
 #else
-		public static bool IsDebug = false;
+        public static bool IsDebug = false;
 #endif
-		
+
 #if UNITY_EDITOR && !ASYNC
-		public static bool IsAsync = false;
+        public static bool IsAsync = false;
 #else
         public static bool IsAsync = true;
 #endif
-		
+
 #if UNITY_EDITOR
-		public static bool IsEditor = true;
+        public static bool IsEditor = true;
 #else
         public static bool IsEditor = false;
 #endif
-		
+
 #if ENABLE_DLL
-		public static bool EnableDll = true;
+        public static bool EnableDll = true;
 #else
         public static bool EnableDll = false;
 #endif
-		
+
 #if ENABLE_VIEW
-		public static bool EnableView = true;
+        public static bool EnableView = true;
 #else
-		public static bool EnableView = false;
+        public static bool EnableView = false;
 #endif
-		
+
 #if ENABLE_IL2CPP
-		public static bool EnableIL2CPP = true;
+        public static bool EnableIL2CPP = true;
 #else
-		public static bool EnableIL2CPP = false;
+        public static bool EnableIL2CPP = false;
 #endif
-		
-		public static UnityEngine.Object LoadAssetAtPath(string s)
-		{
-#if UNITY_EDITOR	
-			return UnityEditor.AssetDatabase.LoadAssetAtPath<UnityEngine.Object>(s);
-#else
-			return null;
-#endif
-		}
-		
-		public static string[] GetAssetPathsFromAssetBundle(string assetBundleName)
-		{
-#if UNITY_EDITOR	
-			return UnityEditor.AssetDatabase.GetAssetPathsFromAssetBundle(assetBundleName);
-#else
-			return new string[0];
-#endif
-		}
-		
-		public static string[] GetAssetBundleDependencies(string assetBundleName, bool v)
-		{
-#if UNITY_EDITOR	
-			return UnityEditor.AssetDatabase.GetAssetBundleDependencies(assetBundleName, v);
-#else
-			return new string[0];
-#endif
-		}
-	}
+    }
 }
