6dbff43d3a2d8bf66febc23dec8c7bbdbc1f99ff
susices <susices@hotmail.com>
2023-11-29 14:35:36 +0800

    修复在关闭EnableDll时 Entity序列化器代码未生成的问题 (#523)

--- Share/Share.SourceGenerator/Generator/ETEntitySerializeFormatterGenerator.cs
+++ Share/Share.SourceGenerator/Generator/ETEntitySerializeFormatterGenerator.cs
@@ -173,7 +173,8 @@ namespace ET
         
         public void OnVisitSyntaxNode(GeneratorSyntaxContext context)
         {
-            if (!AnalyzerHelper.IsSemanticModelNeedAnalyze(context.SemanticModel, UnityCodesPath.UnityModel))
+            if (!AnalyzerHelper.IsAssemblyNeedAnalyze(context.SemanticModel.Compilation.AssemblyName,AnalyzeAssembly.UnityModel) 
+                && !AnalyzerHelper.IsSemanticModelNeedAnalyze(context.SemanticModel, UnityCodesPath.UnityModel))
             {
                 return;
             }
