343830758426635c02b43310ae2f633312c22c59
tanghai <egametang@gmail.com>
2024-01-26 20:16:13 +0800

    搞错，回滚，加上注释，这块容易错误

--- Unity/Assets/Scripts/Hotfix/Server/Demo/Router/HttpGetRouterHandler.cs
+++ Unity/Assets/Scripts/Hotfix/Server/Demo/Router/HttpGetRouterHandler.cs
@@ -13,7 +13,8 @@ namespace ET.Server
             HttpGetRouterResponse response = HttpGetRouterResponse.Create();
             foreach (StartSceneConfig startSceneConfig in StartSceneConfigCategory.Instance.Realms)
             {
-                response.Realms.Add(startSceneConfig.OuterIPPort.ToString());
+                // 这里是要用InnerIP，因为云服务器上realm绑定不了OuterIP的,所以realm的内网外网的socket都是监听内网地址
+                response.Realms.Add(startSceneConfig.InnerIPPort.ToString());
             }
             foreach (StartSceneConfig startSceneConfig in StartSceneConfigCategory.Instance.Routers)
             {
