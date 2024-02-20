ba21c6e4c19cf8c079c9c7968b883df795661aee
EP-Toushirou <45257277+EP-Toushirou@users.noreply.github.com>
2024-02-20 16:09:05 +0800

    proto文件优化及生成文件提交

--- Unity/Assets/Scripts/Model/Generate/Client/Message/ClientMessage_C_1000.cs
+++ Unity/Assets/Scripts/Model/Generate/Client/Message/ClientMessage_C_1000.cs
@@ -1,81 +1,92 @@
-using ET;
 using MemoryPack;
 using System.Collections.Generic;
+
 namespace ET
 {
-// using
-	[ResponseType(nameof(NetClient2Main_Login))]
-	[Message(ClientMessage.Main2NetClient_Login)]
-	[MemoryPackable]
-	public partial class Main2NetClient_Login: MessageObject, IRequest
-	{
-		public static Main2NetClient_Login Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(Main2NetClient_Login), isFromPool) as Main2NetClient_Login; 
-		}
+    [MemoryPackable]
+    [Message(ClientMessage.Main2NetClient_Login)]
+    [ResponseType(nameof(NetClient2Main_Login))]
+    public partial class Main2NetClient_Login : MessageObject, IRequest
+    {
+        public static Main2NetClient_Login Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(Main2NetClient_Login), isFromPool) as Main2NetClient_Login;
+        }
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-		[MemoryPackOrder(1)]
-		public int OwnerFiberId { get; set; }
+        [MemoryPackOrder(1)]
+        public int OwnerFiberId { get; set; }
 
-		[MemoryPackOrder(2)]
-		public string Account { get; set; }
+        /// <summary>
+        /// 账号
+        /// </summary>
+        [MemoryPackOrder(2)]
+        public string Account { get; set; }
 
-		[MemoryPackOrder(3)]
-		public string Password { get; set; }
+        /// <summary>
+        /// 密码
+        /// </summary>
+        [MemoryPackOrder(3)]
+        public string Password { get; set; }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.RpcId = default;
-			this.OwnerFiberId = default;
-			this.Account = default;
-			this.Password = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-	}
+            this.RpcId = default;
+            this.OwnerFiberId = default;
+            this.Account = default;
+            this.Password = default;
 
-	[Message(ClientMessage.NetClient2Main_Login)]
-	[MemoryPackable]
-	public partial class NetClient2Main_Login: MessageObject, IResponse
-	{
-		public static NetClient2Main_Login Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(NetClient2Main_Login), isFromPool) as NetClient2Main_Login; 
-		}
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+    [MemoryPackable]
+    [Message(ClientMessage.NetClient2Main_Login)]
+    public partial class NetClient2Main_Login : MessageObject, IResponse
+    {
+        public static NetClient2Main_Login Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(NetClient2Main_Login), isFromPool) as NetClient2Main_Login;
+        }
 
-		[MemoryPackOrder(1)]
-		public int Error { get; set; }
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-		[MemoryPackOrder(2)]
-		public string Message { get; set; }
+        [MemoryPackOrder(1)]
+        public int Error { get; set; }
 
-		[MemoryPackOrder(3)]
-		public long PlayerId { get; set; }
+        [MemoryPackOrder(2)]
+        public string Message { get; set; }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.RpcId = default;
-			this.Error = default;
-			this.Message = default;
-			this.PlayerId = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+        [MemoryPackOrder(3)]
+        public long PlayerId { get; set; }
 
-	}
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-	public static class ClientMessage
-	{
-		 public const ushort Main2NetClient_Login = 1001;
-		 public const ushort NetClient2Main_Login = 1002;
-	}
+            this.RpcId = default;
+            this.Error = default;
+            this.Message = default;
+            this.PlayerId = default;
+
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
+
+    public static class ClientMessage
+    {
+        public const ushort Main2NetClient_Login = 1001;
+        public const ushort NetClient2Main_Login = 1002;
+    }
 }
--- Unity/Assets/Scripts/Model/Generate/Server/Message/LockStepOuter_C_11001.cs
+++ Unity/Assets/Scripts/Model/Generate/Server/Message/LockStepOuter_C_11001.cs
@@ -1,342 +1,386 @@
-using ET;
 using MemoryPack;
 using System.Collections.Generic;
+
 namespace ET
 {
-	[ResponseType(nameof(G2C_Match))]
-	[Message(LockStepOuter.C2G_Match)]
-	[MemoryPackable]
-	public partial class C2G_Match: MessageObject, ISessionRequest
-	{
-		public static C2G_Match Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(C2G_Match), isFromPool) as C2G_Match; 
-		}
+    [MemoryPackable]
+    [Message(LockStepOuter.C2G_Match)]
+    [ResponseType(nameof(G2C_Match))]
+    public partial class C2G_Match : MessageObject, ISessionRequest
+    {
+        public static C2G_Match Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(C2G_Match), isFromPool) as C2G_Match;
+        }
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.RpcId = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-	}
+            this.RpcId = default;
 
-	[Message(LockStepOuter.G2C_Match)]
-	[MemoryPackable]
-	public partial class G2C_Match: MessageObject, ISessionResponse
-	{
-		public static G2C_Match Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(G2C_Match), isFromPool) as G2C_Match; 
-		}
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+    [MemoryPackable]
+    [Message(LockStepOuter.G2C_Match)]
+    public partial class G2C_Match : MessageObject, ISessionResponse
+    {
+        public static G2C_Match Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(G2C_Match), isFromPool) as G2C_Match;
+        }
 
-		[MemoryPackOrder(1)]
-		public int Error { get; set; }
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-		[MemoryPackOrder(2)]
-		public string Message { get; set; }
+        [MemoryPackOrder(1)]
+        public int Error { get; set; }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.RpcId = default;
-			this.Error = default;
-			this.Message = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+        [MemoryPackOrder(2)]
+        public string Message { get; set; }
 
-	}
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-// 匹配成功，通知客户端切换场景
-	[Message(LockStepOuter.Match2G_NotifyMatchSuccess)]
-	[MemoryPackable]
-	public partial class Match2G_NotifyMatchSuccess: MessageObject, IMessage
-	{
-		public static Match2G_NotifyMatchSuccess Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(Match2G_NotifyMatchSuccess), isFromPool) as Match2G_NotifyMatchSuccess; 
-		}
+            this.RpcId = default;
+            this.Error = default;
+            this.Message = default;
 
-		[MemoryPackOrder(0)]
-		public int RpcId { get; set; }
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-// 房间的ActorId
-		[MemoryPackOrder(1)]
-		public ActorId ActorId { get; set; }
+    /// <summary>
+    /// 匹配成功，通知客户端切换场景
+    /// </summary>
+    [MemoryPackable]
+    [Message(LockStepOuter.Match2G_NotifyMatchSuccess)]
+    public partial class Match2G_NotifyMatchSuccess : MessageObject, IMessage
+    {
+        public static Match2G_NotifyMatchSuccess Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(Match2G_NotifyMatchSuccess), isFromPool) as Match2G_NotifyMatchSuccess;
+        }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.RpcId = default;
-			this.ActorId = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+        [MemoryPackOrder(0)]
+        public int RpcId { get; set; }
 
-	}
+        /// <summary>
+        /// 房间的ActorId
+        /// </summary>
+        [MemoryPackOrder(1)]
+        public ActorId ActorId { get; set; }
 
-// 客户端通知房间切换场景完成
-	[Message(LockStepOuter.C2Room_ChangeSceneFinish)]
-	[MemoryPackable]
-	public partial class C2Room_ChangeSceneFinish: MessageObject, IRoomMessage
-	{
-		public static C2Room_ChangeSceneFinish Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(C2Room_ChangeSceneFinish), isFromPool) as C2Room_ChangeSceneFinish; 
-		}
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-		[MemoryPackOrder(0)]
-		public long PlayerId { get; set; }
+            this.RpcId = default;
+            this.ActorId = default;
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.PlayerId = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-	}
+    /// <summary>
+    /// 客户端通知房间切换场景完成
+    /// </summary>
+    [MemoryPackable]
+    [Message(LockStepOuter.C2Room_ChangeSceneFinish)]
+    public partial class C2Room_ChangeSceneFinish : MessageObject, IRoomMessage
+    {
+        public static C2Room_ChangeSceneFinish Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(C2Room_ChangeSceneFinish), isFromPool) as C2Room_ChangeSceneFinish;
+        }
 
-	[Message(LockStepOuter.LockStepUnitInfo)]
-	[MemoryPackable]
-	public partial class LockStepUnitInfo: MessageObject
-	{
-		public static LockStepUnitInfo Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(LockStepUnitInfo), isFromPool) as LockStepUnitInfo; 
-		}
+        [MemoryPackOrder(0)]
+        public long PlayerId { get; set; }
 
-		[MemoryPackOrder(0)]
-		public long PlayerId { get; set; }
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-		[MemoryPackOrder(1)]
-		public TrueSync.TSVector Position { get; set; }
+            this.PlayerId = default;
 
-		[MemoryPackOrder(2)]
-		public TrueSync.TSQuaternion Rotation { get; set; }
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.PlayerId = default;
-			this.Position = default;
-			this.Rotation = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+    [MemoryPackable]
+    [Message(LockStepOuter.LockStepUnitInfo)]
+    public partial class LockStepUnitInfo : MessageObject
+    {
+        public static LockStepUnitInfo Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(LockStepUnitInfo), isFromPool) as LockStepUnitInfo;
+        }
 
-	}
+        [MemoryPackOrder(0)]
+        public long PlayerId { get; set; }
 
-// 房间通知客户端进入战斗
-	[Message(LockStepOuter.Room2C_Start)]
-	[MemoryPackable]
-	public partial class Room2C_Start: MessageObject, IMessage
-	{
-		public static Room2C_Start Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(Room2C_Start), isFromPool) as Room2C_Start; 
-		}
+        [MemoryPackOrder(1)]
+        public TrueSync.TSVector Position { get; set; }
 
-		[MemoryPackOrder(0)]
-		public long StartTime { get; set; }
+        [MemoryPackOrder(2)]
+        public TrueSync.TSQuaternion Rotation { get; set; }
 
-		[MemoryPackOrder(1)]
-		public List<LockStepUnitInfo> UnitInfo { get; set; } = new();
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.StartTime = default;
-			this.UnitInfo.Clear();
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+            this.PlayerId = default;
+            this.Position = default;
+            this.Rotation = default;
 
-	}
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-	[Message(LockStepOuter.FrameMessage)]
-	[MemoryPackable]
-	public partial class FrameMessage: MessageObject, IMessage
-	{
-		public static FrameMessage Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(FrameMessage), isFromPool) as FrameMessage; 
-		}
+    /// <summary>
+    /// 房间通知客户端进入战斗
+    /// </summary>
+    [MemoryPackable]
+    [Message(LockStepOuter.Room2C_Start)]
+    public partial class Room2C_Start : MessageObject, IMessage
+    {
+        public static Room2C_Start Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(Room2C_Start), isFromPool) as Room2C_Start;
+        }
 
-		[MemoryPackOrder(0)]
-		public int Frame { get; set; }
+        [MemoryPackOrder(0)]
+        public long StartTime { get; set; }
 
-		[MemoryPackOrder(1)]
-		public long PlayerId { get; set; }
+        [MemoryPackOrder(1)]
+        public List<LockStepUnitInfo> UnitInfo { get; set; } = new();
 
-		[MemoryPackOrder(2)]
-		public LSInput Input { get; set; }
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.Frame = default;
-			this.PlayerId = default;
-			this.Input = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+            this.StartTime = default;
+            this.UnitInfo.Clear();
 
-	}
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-	[Message(LockStepOuter.OneFrameInputs)]
-	[MemoryPackable]
-	public partial class OneFrameInputs: MessageObject, IMessage
-	{
-		public static OneFrameInputs Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(OneFrameInputs), isFromPool) as OneFrameInputs; 
-		}
+    [MemoryPackable]
+    [Message(LockStepOuter.FrameMessage)]
+    public partial class FrameMessage : MessageObject, IMessage
+    {
+        public static FrameMessage Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(FrameMessage), isFromPool) as FrameMessage;
+        }
 
-		[MongoDB.Bson.Serialization.Attributes.BsonDictionaryOptions(MongoDB.Bson.Serialization.Options.DictionaryRepresentation.ArrayOfArrays)]
-		[MemoryPackOrder(1)]
-		public Dictionary<long, LSInput> Inputs { get; set; } = new();
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.Inputs.Clear();
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+        [MemoryPackOrder(0)]
+        public int Frame { get; set; }
 
-	}
+        [MemoryPackOrder(1)]
+        public long PlayerId { get; set; }
 
-	[Message(LockStepOuter.Room2C_AdjustUpdateTime)]
-	[MemoryPackable]
-	public partial class Room2C_AdjustUpdateTime: MessageObject, IMessage
-	{
-		public static Room2C_AdjustUpdateTime Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(Room2C_AdjustUpdateTime), isFromPool) as Room2C_AdjustUpdateTime; 
-		}
+        [MemoryPackOrder(2)]
+        public LSInput Input { get; set; }
 
-		[MemoryPackOrder(0)]
-		public int DiffTime { get; set; }
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.DiffTime = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+            this.Frame = default;
+            this.PlayerId = default;
+            this.Input = default;
 
-	}
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-	[Message(LockStepOuter.C2Room_CheckHash)]
-	[MemoryPackable]
-	public partial class C2Room_CheckHash: MessageObject, IRoomMessage
-	{
-		public static C2Room_CheckHash Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(C2Room_CheckHash), isFromPool) as C2Room_CheckHash; 
-		}
+    [MemoryPackable]
+    [Message(LockStepOuter.OneFrameInputs)]
+    public partial class OneFrameInputs : MessageObject, IMessage
+    {
+        public static OneFrameInputs Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(OneFrameInputs), isFromPool) as OneFrameInputs;
+        }
 
-		[MemoryPackOrder(0)]
-		public long PlayerId { get; set; }
+        [MongoDB.Bson.Serialization.Attributes.BsonDictionaryOptions(MongoDB.Bson.Serialization.Options.DictionaryRepresentation.ArrayOfArrays)]
+        [MemoryPackOrder(1)]
+        public Dictionary<long, LSInput> Inputs { get; set; } = new();
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-		[MemoryPackOrder(1)]
-		public int Frame { get; set; }
+            this.Inputs.Clear();
 
-		[MemoryPackOrder(2)]
-		public long Hash { get; set; }
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.PlayerId = default;
-			this.Frame = default;
-			this.Hash = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+    [MemoryPackable]
+    [Message(LockStepOuter.Room2C_AdjustUpdateTime)]
+    public partial class Room2C_AdjustUpdateTime : MessageObject, IMessage
+    {
+        public static Room2C_AdjustUpdateTime Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(Room2C_AdjustUpdateTime), isFromPool) as Room2C_AdjustUpdateTime;
+        }
 
-	}
+        [MemoryPackOrder(0)]
+        public int DiffTime { get; set; }
 
-	[Message(LockStepOuter.Room2C_CheckHashFail)]
-	[MemoryPackable]
-	public partial class Room2C_CheckHashFail: MessageObject, IMessage
-	{
-		public static Room2C_CheckHashFail Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(Room2C_CheckHashFail), isFromPool) as Room2C_CheckHashFail; 
-		}
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-		[MemoryPackOrder(0)]
-		public int Frame { get; set; }
+            this.DiffTime = default;
 
-		[MemoryPackOrder(1)]
-		public byte[] LSWorldBytes { get; set; }
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.Frame = default;
-			this.LSWorldBytes = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+    [MemoryPackable]
+    [Message(LockStepOuter.C2Room_CheckHash)]
+    public partial class C2Room_CheckHash : MessageObject, IRoomMessage
+    {
+        public static C2Room_CheckHash Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(C2Room_CheckHash), isFromPool) as C2Room_CheckHash;
+        }
 
-	}
+        [MemoryPackOrder(0)]
+        public long PlayerId { get; set; }
 
-	[Message(LockStepOuter.G2C_Reconnect)]
-	[MemoryPackable]
-	public partial class G2C_Reconnect: MessageObject, IMessage
-	{
-		public static G2C_Reconnect Create(bool isFromPool = false) 
-		{ 
-			return ObjectPool.Instance.Fetch(typeof(G2C_Reconnect), isFromPool) as G2C_Reconnect; 
-		}
+        [MemoryPackOrder(1)]
+        public int Frame { get; set; }
 
-		[MemoryPackOrder(0)]
-		public long StartTime { get; set; }
+        [MemoryPackOrder(2)]
+        public long Hash { get; set; }
 
-		[MemoryPackOrder(1)]
-		public List<LockStepUnitInfo> UnitInfos { get; set; } = new();
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
 
-		[MemoryPackOrder(2)]
-		public int Frame { get; set; }
+            this.PlayerId = default;
+            this.Frame = default;
+            this.Hash = default;
 
-		public override void Dispose() 
-		{
-			if (!this.IsFromPool) { return; }
-			this.StartTime = default;
-			this.UnitInfos.Clear();
-			this.Frame = default;
-			
-			ObjectPool.Instance.Recycle(this); 
-		}
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
 
-	}
+    [MemoryPackable]
+    [Message(LockStepOuter.Room2C_CheckHashFail)]
+    public partial class Room2C_CheckHashFail : MessageObject, IMessage
+    {
+        public static Room2C_CheckHashFail Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(Room2C_CheckHashFail), isFromPool) as Room2C_CheckHashFail;
+        }
 
-	public static class LockStepOuter
-	{
-		 public const ushort C2G_Match = 11002;
-		 public const ushort G2C_Match = 11003;
-		 public const ushort Match2G_NotifyMatchSuccess = 11004;
-		 public const ushort C2Room_ChangeSceneFinish = 11005;
-		 public const ushort LockStepUnitInfo = 11006;
-		 public const ushort Room2C_Start = 11007;
-		 public const ushort FrameMessage = 11008;
-		 public const ushort OneFrameInputs = 11009;
-		 public const ushort Room2C_AdjustUpdateTime = 11010;
-		 public const ushort C2Room_CheckHash = 11011;
-		 public const ushort Room2C_CheckHashFail = 11012;
-		 public const ushort G2C_Reconnect = 11013;
-	}
+        [MemoryPackOrder(0)]
+        public int Frame { get; set; }
+
+        [MemoryPackOrder(1)]
+        public byte[] LSWorldBytes { get; set; }
+
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
+
+            this.Frame = default;
+            this.LSWorldBytes = default;
+
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
+
+    [MemoryPackable]
+    [Message(LockStepOuter.G2C_Reconnect)]
+    public partial class G2C_Reconnect : MessageObject, IMessage
+    {
+        public static G2C_Reconnect Create(bool isFromPool = false)
+        {
+            return ObjectPool.Instance.Fetch(typeof(G2C_Reconnect), isFromPool) as G2C_Reconnect;
+        }
+
+        [MemoryPackOrder(0)]
+        public long StartTime { get; set; }
+
+        [MemoryPackOrder(1)]
+        public List<LockStepUnitInfo> UnitInfos { get; set; } = new();
+
+        [MemoryPackOrder(2)]
+        public int Frame { get; set; }
+
+        public override void Dispose()
+        {
+            if (!this.IsFromPool)
+            {
+                return;
+            }
+
+            this.StartTime = default;
+            this.UnitInfos.Clear();
+            this.Frame = default;
+
+            ObjectPool.Instance.Recycle(this);
+        }
+    }
+
+    public static class LockStepOuter
+    {
+        public const ushort C2G_Match = 11002;
+        public const ushort G2C_Match = 11003;
+        public const ushort Match2G_NotifyMatchSuccess = 11004;
+        public const ushort C2Room_ChangeSceneFinish = 11005;
+        public const ushort LockStepUnitInfo = 11006;
+        public const ushort Room2C_Start = 11007;
+        public const ushort FrameMessage = 11008;
+        public const ushort OneFrameInputs = 11009;
+        public const ushort Room2C_AdjustUpdateTime = 11010;
+        public const ushort C2Room_CheckHash = 11011;
+        public const ushort Room2C_CheckHashFail = 11012;
+        public const ushort G2C_Reconnect = 11013;
+    }
 }
