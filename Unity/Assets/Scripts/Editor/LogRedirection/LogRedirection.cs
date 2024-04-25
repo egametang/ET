bfe29831895d3f2cc93d68d43dec7f62b8550756
王二 <18530117331@163.com>
2024-04-25 16:36:04 +0800

    增加编辑器Catch Exception的报错堆栈跳转 (#591)
    Co-authored-by: zhangguoju <zhangguoju@123.com>

--- Unity/Assets/Scripts/Editor/LogRedirection/LogRedirection.cs
+++ Unity/Assets/Scripts/Editor/LogRedirection/LogRedirection.cs
@@ -26,7 +26,16 @@ namespace ET
             string codePath = AssetDatabase.GetAssetPath(instanceID);
             if (logFileRegex.IsMatch(codePath))
             {
-                Match stackLineMatch = Regex.Match(GetStackTrace(), @"\(at (.+):([0-9]+)\)");
+                var content = GetStackTrace();
+                // 如果有手动输入的地址，就跳过
+                var hrefMath = Regex.Match(content, @"<a href=""(.*?)"" line=""(\w+)\"">.*?</a>");
+                if (hrefMath.Success)
+                {
+                    OpenIDE(hrefMath.Groups[1].Value, int.Parse(hrefMath.Groups[2].Value));
+                    return true;
+                }
+                
+                Match stackLineMatch = Regex.Match(content, @"\(at (.+):([0-9]+)\)");
                 while (stackLineMatch.Success)
                 {
                     codePath = stackLineMatch.Groups[1].Value;
