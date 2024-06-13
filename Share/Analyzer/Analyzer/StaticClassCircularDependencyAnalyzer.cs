66a33dbf68a073770d4effd180b02bbfb092ce24
wuwenchao <susices@hotmail.com>
2024-06-13 17:32:51 +0800

    优化环形依赖分析器的日志 输出静态类调用链和相关的调用函数

--- Share/Analyzer/Analyzer/StaticClassCircularDependencyAnalyzer.cs
+++ Share/Analyzer/Analyzer/StaticClassCircularDependencyAnalyzer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Concurrent;
+﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
@@ -15,7 +16,7 @@ namespace ET.Analyzer
     {
         private const string Title = "静态类之间禁止环形依赖";
 
-        private const string MessageFormat = "ET0013 静态类函数引用存在环形依赖 请修改为单向依赖 静态类{0}被 静态类{1}引用";
+        private const string MessageFormat = "ET0013 静态类函数引用存在环形依赖 请修改为单向依赖 {0}";
 
         private const string Description = "静态类之间禁止环形依赖.";
 
@@ -43,11 +44,12 @@ namespace ET.Analyzer
 
         private void CompilationStartAnalysis(CompilationStartAnalysisContext context)
         {
-            var dependencyMap = new ConcurrentDictionary<string, HashSet<string>>();
+            var dependencyMap = new ConcurrentDictionary<string, Dictionary<string,List<InvocationExpressionSyntax>>>();
             var staticClassSet = new HashSet<string>();
             
             if (AnalyzerHelper.IsAssemblyNeedAnalyze(context.Compilation.AssemblyName, AnalyzeAssembly.AllHotfix))
             {
+                
                 context.RegisterSyntaxNodeAction(analysisContext => { this.StaticClassDependencyAnalyze(analysisContext, dependencyMap, staticClassSet); }, SyntaxKind.InvocationExpression);
                 context.RegisterCompilationEndAction(analysisContext => { this.CircularDependencyAnalyze(analysisContext, dependencyMap, staticClassSet); });
             }
@@ -57,7 +59,7 @@ namespace ET.Analyzer
         /// <summary>
         /// 静态类依赖分析 构建depedencyMap
         /// </summary>
-        private void StaticClassDependencyAnalyze(SyntaxNodeAnalysisContext context, ConcurrentDictionary<string, HashSet<string>> dependencyMap,
+        private void StaticClassDependencyAnalyze(SyntaxNodeAnalysisContext context, ConcurrentDictionary<string, Dictionary<string,List<InvocationExpressionSyntax>>> dependencyMap,
         HashSet<string> staticClassSet)
         {
             if (!(context.Node is InvocationExpressionSyntax invocationExpressionSyntax))
@@ -109,24 +111,26 @@ namespace ET.Analyzer
 
             if (!dependencyMap.ContainsKey(methodClassTypeName))
             {
-                dependencyMap[methodClassTypeName] = new HashSet<string>();
+                dependencyMap[methodClassTypeName] = new ();
             }
 
-            var set = dependencyMap[methodClassTypeName];
-            lock (set)
+            var dic = dependencyMap[methodClassTypeName];
+            lock (dic)
             {
-                if (!set.Contains(selfClassTypeName))
+                List<InvocationExpressionSyntax> invocationExpressionSyntaxes;
+                if (!dic.TryGetValue(selfClassTypeName,out invocationExpressionSyntaxes))
                 {
-                    set.Add(selfClassTypeName);
+                    invocationExpressionSyntaxes = new List<InvocationExpressionSyntax>();
+                    dic.Add(selfClassTypeName,invocationExpressionSyntaxes);
                 }
+                invocationExpressionSyntaxes.Add(invocationExpressionSyntax);
             }
-            
         }
 
         /// <summary>
         /// 环形依赖分析
         /// </summary>
-        private void CircularDependencyAnalyze(CompilationAnalysisContext context, ConcurrentDictionary<string, HashSet<string>> dependencyMap,
+        private void CircularDependencyAnalyze(CompilationAnalysisContext context, ConcurrentDictionary<string, Dictionary<string,List<InvocationExpressionSyntax>>> dependencyMap,
         HashSet<string> staticClassSet)
         {
             
@@ -138,7 +142,7 @@ namespace ET.Analyzer
                 {
                     foreach (var dependency in dependencyMap)
                     {
-                        if (dependency.Value.Contains(noDependencyStaticClass))
+                        if (dependency.Value.ContainsKey(noDependencyStaticClass))
                         {
                             dependency.Value.Remove(noDependencyStaticClass);
                         }
@@ -152,7 +156,7 @@ namespace ET.Analyzer
                 }
             }
             
-            var staticClassDependencyMap = new ConcurrentDictionary<string, HashSet<string>>();
+            var staticClassDependencyMap = new ConcurrentDictionary<string, Dictionary<string,List<InvocationExpressionSyntax>>>();
             foreach (string? staticClass in staticClassSet)
             {
                 staticClassDependencyMap[staticClass] = dependencyMap[staticClass];
@@ -174,29 +178,21 @@ namespace ET.Analyzer
 
             if (staticClassSet.Count > 0)
             {
-                foreach (string? staticClass in staticClassSet)
+                // 找出所有的环
+                var visited = new HashSet<string>();
+                var stack = new Stack<string>();
+                var path = new List<string>();
+                foreach (var staticClass in staticClassSet)
                 {
-                    Diagnostic diagnostic = Diagnostic.Create(Rule, null, staticClass,
-                        FormatSet(dependencyMap[staticClass]));
-                    context.ReportDiagnostic(diagnostic);
+                    this.FindAllCycles(staticClass, staticClassDependencyMap, visited, stack, path, context);
                 }
-            }
-
-            string FormatSet(HashSet<string> hashSet)
-            {
-                StringBuilder stringBuilder = new StringBuilder();
-                foreach (string? value in hashSet)
-                {
-                    stringBuilder.Append($"{value} ");
-                }
-                return stringBuilder.ToString();
             }
         }
         
         /// <summary>
         /// 获取没有被任何其他静态类引用的静态类
         /// </summary>
-        private string GetClassNotReferencedByOtherStaticClass(ConcurrentDictionary<string, HashSet<string>> dependencyMap,
+        private string GetClassNotReferencedByOtherStaticClass(ConcurrentDictionary<string, Dictionary<string,List<InvocationExpressionSyntax>>> dependencyMap,
         HashSet<string> staticClassSet)
         {
             foreach (string? staticClass in staticClassSet)
@@ -214,13 +210,13 @@ namespace ET.Analyzer
         /// <summary>
         /// 获取没有引用任何其他静态类的静态类
         /// </summary>
-        private string GetClassNotReferenceAnyStaticClass(ConcurrentDictionary<string, HashSet<string>> dependencyMap,
+        private string GetClassNotReferenceAnyStaticClass(ConcurrentDictionary<string, Dictionary<string,List<InvocationExpressionSyntax>>> dependencyMap,
         HashSet<string> staticClassSet)
         {
             foreach (string? staticClass in staticClassSet)
             {
-                var result = dependencyMap.Where(x => x.Value.Contains(staticClass));
-                if (result.Count() == 0)
+                var result = dependencyMap.Where(x => x.Value.ContainsKey(staticClass));
+                if (!result.Any())
                 {
                     return staticClass;
                 }
@@ -228,5 +224,58 @@ namespace ET.Analyzer
 
             return string.Empty;
         }
+        
+        private void FindAllCycles(string currentClass, ConcurrentDictionary<string, Dictionary<string, List<InvocationExpressionSyntax>>> dependencyMap,
+        HashSet<string> visited, Stack<string> stack, List<string> path, CompilationAnalysisContext context)
+        {
+            if (stack.Contains(currentClass))
+            {
+                int index = path.IndexOf(currentClass);
+                var cyclePath = path.Skip(index).ToList();
+                cyclePath.Add(currentClass);
+                cyclePath.Reverse();
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < cyclePath.Count-1; i++)
+                {
+                    var invokeClass = cyclePath[i];
+                    var beInvokedClass = cyclePath[i + 1];
+
+                    sb.Append($" {invokeClass} -> {beInvokedClass} invocation:( ");
+                    foreach (var invocation in dependencyMap[beInvokedClass][invokeClass])
+                    {
+                        sb.Append(invocation.ToString());
+                        sb.Append(" ");
+                    }
+
+                    sb.Append(")");
+                }
+                
+                var diagnostic = Diagnostic.Create(Rule, Location.None, sb);
+                context.ReportDiagnostic(diagnostic);
+                return;
+            }
+
+            if (visited.Contains(currentClass))
+            {
+                return;
+            }
+
+            visited.Add(currentClass);
+            stack.Push(currentClass);
+            path.Add(currentClass);
+
+            if (dependencyMap.ContainsKey(currentClass))
+            {
+                foreach (var dependency in dependencyMap[currentClass].Keys)
+                {
+                    this.FindAllCycles(dependency, dependencyMap, visited, stack, path, context);
+                }
+            }
+
+            stack.Pop();
+            path.RemoveAt(path.Count - 1);
+        }
+        
     }
 }
