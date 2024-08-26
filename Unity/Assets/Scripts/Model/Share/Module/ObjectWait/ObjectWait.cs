6b8fc423fa912be4ba63cb5cea5a5710838d8da3
夏到了 <lin.xr@foxmail.com>
2024-08-26 23:17:05 +0800

    ObjectWait支持多个Wait (#629)

--- Unity/Assets/Scripts/Model/Share/Module/ObjectWait/ObjectWait.cs
+++ Unity/Assets/Scripts/Model/Share/Module/ObjectWait/ObjectWait.cs
@@ -83,7 +83,7 @@ namespace ET
         {
             ResultCallback<T> tcs = new ResultCallback<T>();
             Type type = typeof (T);
-            self.tcss.Add(type, tcs);
+            self.Add(type, tcs);
 
             void CancelAction()
             {
@@ -106,6 +106,7 @@ namespace ET
         public static async ETTask<T> Wait<T>(this ObjectWait self, int timeout, ETCancellationToken cancellationToken = null) where T : struct, IWaitType
         {
             ResultCallback<T> tcs = new ResultCallback<T>();
+            Type type = typeof(T);
             async ETTask WaitTimeout()
             {
                 await self.Root().GetComponent<TimerComponent>().WaitAsync(timeout, cancellationToken);
@@ -117,12 +118,18 @@ namespace ET
                 {
                     return;
                 }
-                self.Notify(new T() { Error = WaitTypeError.Timeout });
+                
+                if (!self.tcss.TryGetValue(type, out var tcsList))
+                {
+                    return;
+                }
+                tcsList.Remove(tcs);
+                tcs.SetResult(new T() { Error = WaitTypeError.Timeout });
             }
             
             WaitTimeout().Coroutine();
             
-            self.tcss.Add(typeof (T), tcs);
+            self.Add(type, tcs);
             
             void CancelAction()
             {
@@ -145,19 +152,35 @@ namespace ET
         public static void Notify<T>(this ObjectWait self, T obj) where T : struct, IWaitType
         {
             Type type = typeof (T);
-            if (!self.tcss.TryGetValue(type, out object tcs))
+            if (!self.tcss.TryGetValue(type, out var tcsList) || tcsList.Count == 0)
             {
                 return;
             }
 
-            self.tcss.Remove(type);
-            ((ResultCallback<T>) tcs).SetResult(obj);
+            foreach(var tcs in tcsList)
+            {
+                ((ResultCallback<T>) tcs).SetResult(obj);
+            }
+            tcsList.Clear();
+        }
+
+
+        private static void Add(this ObjectWait self, Type type, object obj)
+        {
+            if (self.tcss.TryGetValue(type, out var list))
+            {
+                list.Add(obj);
+            }
+            else
+            {
+                self.tcss.Add(type, new List<object> { obj });
+            }
         }
     }
 
     [ComponentOf]
     public class ObjectWait: Entity, IAwake, IDestroy
     {
-        public Dictionary<Type, object> tcss = new Dictionary<Type, object>();
+        public Dictionary<Type, List<object>> tcss = new();
     }
 }
